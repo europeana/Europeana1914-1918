d7d0a52990dc974b3d135def012dc848179480b1
richard doe <richard.doe@rwdit.net>
2012-01-30 11:16:43 +0000

    Added checkbox & select support to metadata cloning JS.

--- public/javascripts/clone-metadata.js
+++ public/javascripts/clone-metadata.js
@@ -4,11 +4,25 @@ $(document).ready(function(){
     cloneButton.click(function() {
       $('#attachment_title').val(contributionTitle);
       for (var key in contributionMetadata['metadata_record']) {
+        var fieldValue = contributionMetadata['metadata_record'][key];
+        
         // if metadata value is an array, look for individual input elements with ids
         // like 'attachment_metadata_attributes_field_keywords_term_ids_63'
-        // and check/enable them
-      
-        var fieldValue = contributionMetadata['metadata_record'][key];
+        // and check them
+        if(Object.prototype.toString.call(fieldValue) === '[object Array]') {
+          $('*[id^="attachment_metadata_attributes_' + key + '_"]').removeAttr('checked');
+          for (var i = 0; i < fieldValue.length; i++) {
+            $('#attachment_metadata_attributes_' + key + '_' + fieldValue[i]).each(function(index) {
+              var fieldType = $(this).attr('type').toLowerCase();
+              if (fieldType == 'checkbox') {
+                $(this).attr('checked', 'checked');
+              } else {
+//                alert(fieldType);
+              }
+            });
+          }
+        } 
+          
         $('#attachment_metadata_attributes_' + key).each(function(index) {
           var fieldTagName = this.tagName.toLowerCase();
           if (fieldTagName == 'input') {
@@ -16,12 +30,14 @@ $(document).ready(function(){
             if (fieldType == 'text') {
               $(this).val(fieldValue);
             } else { 
-              //alert(fieldType); 
+//              alert(fieldType); 
             }
           } else if (fieldTagName == 'textarea') {
             $(this).text(fieldValue);
+          } else if (fieldTagName == 'select') {
+            $('option[value="' + fieldValue + '"]', this).attr('selected', 'selected');
           } else { 
-            // alert(fieldTagName + ': ' + fieldValue); 
+//            alert(fieldTagName + ': ' + fieldValue); 
           }
         });
       }
