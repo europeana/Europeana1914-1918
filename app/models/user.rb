33b8dd14106b20f4df5ce054308d80c48fc54712
richard doe <richard.doe@rwdit.net>
2012-02-16 13:21:32 +0000

    Add username field and use it for sign-in.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
-# Authenticated users.
+##
+# Application user.
+#
 # Uses Devise for authentication and Aegis for role-based authorization.
-# TODO: Prevent deletion of last admin user.
 class User < ActiveRecord::Base
   # Devise authentication
-  devise :database_authenticatable, :recoverable, :validatable, :registerable
+  devise :database_authenticatable, :recoverable, :registerable
   
   # Aegis roles
   has_role :default => 'guest'
@@ -24,16 +25,31 @@ class User < ActiveRecord::Base
     :url => "/images/users/:id/:style/:filename"
 
   accepts_nested_attributes_for :contact
-  attr_accessible :email, :password, :password_confirmation, :contact_attributes, :terms, :picture
+  
+  attr_accessible :username, :email, :password, :password_confirmation, :contact_attributes, :terms, :picture
   
   # Default role for saved accounts is contributor
   before_validation lambda { |u| u.role_name = 'contributor' if u.role_name.to_s == 'guest' }, :on => :create
 
   before_save :sync_email
   
+  validates_presence_of :username
+  validates_uniqueness_of :username
+  validates_length_of :username, :minimum => 6, :allow_blank => true
+  validates_format_of :username, :with => /^[a-zA-Z0-9\-\_\.\@]+$/, :allow_blank => true, :if => Proc.new { |u| u.new_record? || u.username_changed? }
+  
   validates_presence_of :role_name, :contact
   validates_associated :contact
   validates_acceptance_of :terms, :allow_nil => false, :accept => true, :if => Proc.new { |u| u.role_name == 'contributor' }
+  
+  validates_uniqueness_of :email, :scope => Devise.authentication_keys[1..-1], :case_sensitive => false, :allow_blank => true
+  validates_format_of     :email, :with  => Devise.email_regexp, :allow_blank => true
+
+  with_options :if => :password_required? do |v|
+    v.validates_presence_of     :password
+    v.validates_confirmation_of :password
+    v.validates_length_of       :password, :within => Devise.password_length, :allow_blank => true
+  end
   
   validates_attachment_content_type :picture, :content_type => [ 'image/jpeg', 'image/pjpeg', 'image/gif' ], :message => I18n.t('activerecord.errors.models.user.attributes.picture.content_type')
 
@@ -44,6 +60,14 @@ class User < ActiveRecord::Base
     if contact.present?
       contact.email = email
     end
+  end
+
+  protected
+  # Checks whether a password is needed or not. For validations only.
+  # Passwords are always required if it's a new record, or if the password
+  # or confirmation are being set somewhere.
+  def password_required?
+    !persisted? || !password.nil? || !password_confirmation.nil?
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120214103316) do
+ActiveRecord::Schema.define(:version => 20120216100343) do
 
   create_table "attachments", :force => true do |t|
     t.integer  "contribution_id"
@@ -171,9 +171,11 @@ ActiveRecord::Schema.define(:version => 20120214103316) do
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
+    t.string   "username",                           :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
