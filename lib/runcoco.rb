8ce34c0618fff8ecc4ad246170cedb4d29ed4f79
richard doe <richard.doe@rwdit.net>
2012-03-01 16:36:53 +0000

    Fix error logger for production env.

--- lib/runcoco.rb
+++ lib/runcoco.rb
@@ -9,27 +9,21 @@ module RunCoCo
   class FieldNameInvalid < Exception; end # :nodoc:
   class BadRequest < Exception; end # :nodoc:
   class SearchOffline < Exception; end # :nodoc:
-  
-  # Configuration.
-  @@configuration = nil
-  
-  # Error logger.
-  @@error_logger = nil
-  
+
   class << self
     # Returns the error logger.
     #
     # Error log file will be $RAILS_ROOT/log/$RAILS_ENV.error.log,
     # for example "/var/www/runcoco/log/production.error.log".
     def error_logger
-      @@error_logger ||= RunCoCo::ErrorLogger.new
+      @error_logger ||= RunCoCo::ErrorLogger.new
     end
     
     # Returns the configuration object.
     #
     #   RunCoCo.configuration.site_name # => "RunCoCo"
     def configuration
-      @@configuration ||= RunCoCo::Configuration.new
+      @configuration ||= RunCoCo::Configuration.new
     end
   end
 end
