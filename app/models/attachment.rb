d213fc039cb35127485f9233b2ec8e159dbab645
dan entous <d_entous@yahoo.com>
2012-03-27 15:31:54 +0000

    adding some in code comments on how to add new attachments sizes and re-generate them

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -51,8 +51,8 @@ class Attachment < ActiveRecord::Base
   #     e.g. :new_size => "500x500>"
   # and make sure to add the routing to routes.rb
   #    :constraints => { :style => /(thumb|preview|medium|large|full|new_size)/ }
-  # run the following to create/update the medium and large sizes
-  # bundle exec rake paperclip:refresh:thumbnails CLASS=Attachment STYLES=medium,large
+  # run the following to create the new_size version or update it if the dimmensions changed
+  #     bundle exec rake paperclip:refresh:thumbnails CLASS=Attachment STYLES=new_size
   has_attached_file :file,
     :path => ':env_file_root/:access_dir/:class/:id/:contribution_id.:id.:style.:extension',
     :url => "/:class/:id/:contribution_id.:id.:style.:extension",
