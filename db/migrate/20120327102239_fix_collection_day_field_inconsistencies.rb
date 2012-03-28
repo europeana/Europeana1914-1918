ad3e867888eb03ee234d66bc10fb8171e9b85513
richard doe <richard.doe@rwdit.net>
2012-03-28 10:04:25 +0000

    Fix inconsistent collection day values (based on production site dump from 26 March).

---
+++ db/migrate/20120327102239_fix_collection_day_field_inconsistencies.rb
@@ -0,0 +1,27 @@
+class FixCollectionDayFieldInconsistencies < ActiveRecord::Migration
+  def self.up
+    say_with_time "Fixing inconsistent collection day metadata" do
+      {
+        "BE08" => "BE08 ",
+        "FR07" => [ "FR07 (?)", " FR07" ],
+        "KI13" => "KI 13",
+        "AM15" => [ "AM!5", "Am15", "AM;15", "AM" ],
+        "LU16" => "6.03.2012 Luxembourg",
+        "PR17" => [ "PR17 ", "10/03/2012 Preston", "10/03/2012, Preston",
+                    "12/03/2012, Preston", "M Shaw" ],
+        "DU18" => [ "Dublin ", "21 March 2012", "Dublin 21/03/2012", 
+                    "Dublin, National Library of Ireland", 
+                    "Dublin, National Library of Ireland, 21 March 2012", 
+                    "Dublin, National Library of Ireland, 21/03/2012" ]
+      }.each_pair do |to, froms|
+        [ froms ].flatten.each do |from|
+          say "Changing \"#{from}\" to \"#{to}\"", true
+          MetadataRecord.update_all [ 'field_collection_day = ?', to ], [ 'field_collection_day = ?', from ]
+        end
+      end
+    end
+  end
+
+  def self.down
+  end
+end
