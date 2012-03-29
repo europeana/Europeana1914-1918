0d94fc21746ef77e13952c03249c478673de7a2c
richard doe <richard.doe@rwdit.net>
2012-03-29 10:56:12 +0000

    Add editor's pick text and signature fields.

---
+++ db/migrate/20120329102747_add_editors_pick_text_fields.rb
@@ -0,0 +1,29 @@
+class AddEditorsPickTextFields < ActiveRecord::Migration
+  def self.up
+    position = MetadataField.find_by_name('editor_pick').position
+    MetadataField.update_all('position=position+2', [ 'position > ?', position ])
+  
+    say "Adding editor's pick text field"
+    MetadataField.create!(:name => 'editor_pick_text', 
+      :field_type => 'text', :contribution => true, :attachment => false, 
+      :required => false, :cataloguing => true, :searchable => false,
+      :title => "Editor's pick text", :position => position + 1)
+      
+    say "Adding editor's pick signature field"
+    MetadataField.create!(:name => 'editor_pick_sig', 
+      :field_type => 'string', :contribution => true, :attachment => false, 
+      :required => false, :cataloguing => true, :searchable => false,
+      :title => "Editor's pick signature", :position => position + 2)
+  end
+
+  def self.down
+    say "Removing editor's pick text field"
+    MetadataField.find_by_name('editor_pick_text').destroy
+    
+    say "Removing editor's pick signature field"
+    MetadataField.find_by_name('editor_pick_sig').destroy
+    
+    position = MetadataField.find_by_name('editor_pick').position
+    MetadataField.update_all('position=position-2', [ 'position > ?', position ])
+  end
+end
