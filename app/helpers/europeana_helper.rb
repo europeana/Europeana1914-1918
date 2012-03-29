1329d34f6d10ad024ffcb196dafeeb9b57fe0949
richard doe <richard.doe@rwdit.net>
2012-03-29 15:46:04 +0000

    Simplify blog post retrieval helpers with code reuse.

--- app/helpers/europeana_helper.rb
+++ app/helpers/europeana_helper.rb
@@ -1,4 +1,5 @@
 require 'feedzirra'
+require 'hpricot'
 
 module EuropeanaHelper
   
@@ -18,162 +19,102 @@ module EuropeanaHelper
     end
   end
   
-  def editors_picks(locale = nil)
-    if locale.nil?
-      default = true
-      url = "http://thegreatwararchive.blogspot.com/feeds/posts/default/-/en"
-    elsif locale.to_s == 'de'
-      url = "http://thegreatwararchive.blogspot.com/feeds/posts/default/-/De"
-    else
-      url = "http://thegreatwararchive.blogspot.com/feeds/posts/default/-/#{locale.to_s}"
-    end
-    
-    feed = Feedzirra::Feed.fetch_and_parse(url)
-    if feed.respond_to?(:entries) && feed.entries.present?
-      feed.entries
-    elsif default
-      []
-    else
-      editors_picks
-    end
-  end
-  
-  def editors_pick(locale)
-    (picks = editors_picks(locale)) ? picks.first : nil
-  end
-  
-  def news_items(locale = nil)
-    if locale.nil?
-      default = true
-      url = "http://thegreatwararchive.blogspot.com/feeds/posts/default/-/en-news"
-    else
-      url = "http://thegreatwararchive.blogspot.com/feeds/posts/default/-/#{locale.to_s}-news"
-    end
-    
-    feed = Feedzirra::Feed.fetch_and_parse(url)
-    if feed.respond_to?(:entries) && feed.entries.present?
-      feed.entries
-    elsif default
-      nil
-    else
-      news_items
-    end
-  end
-  
-  def news_item(locale)
-    (items = news_items(locale)) ? items.first : nil
-  end
-  
-  def stories_from_the_archive(locale = nil)
-    if locale.nil?
-      default = true
-      url = "http://europeana1914-1918.blogspot.com/feeds/posts/default/-/stories-from-the-archive-en"
-    else
-      url = "http://europeana1914-1918.blogspot.com/feeds/posts/default/-/#{locale.to_s}"
-    end
-    
-    feed = Feedzirra::Feed.fetch_and_parse(url)
-    if feed.respond_to?(:entries) && feed.entries.present?
-      feed.entries
-    elsif default
-      []
-    else
-      stories_from_the_archive
-    end
-  end
-  
-  def tell_us_your_story(locale = nil)
-    if locale.nil?
-      default = true
-      url = "http://europeana1914-1918.blogspot.com/feeds/posts/default/-/tell-us-your-story-en"
-    else
-      url = "http://europeana1914-1918.blogspot.com/feeds/posts/default/-/#{locale.to_s}"
-    end
-    
-    feed = Feedzirra::Feed.fetch_and_parse(url)
-    if feed.respond_to?(:entries) && feed.entries.present?
-      feed.entries
-    elsif default
-      []
-    else
-      tell_us_your_story
-    end
-  end
-  
-  def collection_days(locale = nil)
-    if locale.nil?
-      default = true
-      url = "http://europeana1914-1918.blogspot.com/feeds/posts/default/-/collection-days-en"
-    else
-      url = "http://europeana1914-1918.blogspot.com/feeds/posts/default/-/#{locale.to_s}"
-    end
-    
-    feed = Feedzirra::Feed.fetch_and_parse(url)
-    if feed.respond_to?(:entries) && feed.entries.present?
-      feed.entries
-    elsif default
-      []
-    else
-      collection_days
-    end
-  end
-  
   def relocale_link(link)
     link.sub(/href="\//, 'href="/' + I18n.locale.to_s + '/')
   end
   
-  def explore_by_theme(locale = nil)
-    if locale.nil?
-      default = true
-      url = "http://europeana1914-1918.blogspot.com/feeds/posts/default/-/explore-by-theme-en"
+  ##
+  # Retrieves entries from the Europeana 1914-1918 blog via Atom feed
+  #
+  # An array of {Feedzirra::Parser::AtomEntry} objects is returned. The content
+  # attribute of these objects is run through {Hpricot} so can be searched 
+  # as an HTML document using its methods.
+  #
+  # @example Get the first paragraph in an entry
+  #   (europeana_blog_posts.first.content/"//p").first
+  #
+  # @example Get all links in an entry
+  #   (europeana_blog_posts.first.content/"//a")
+  #
+  # @param [Hash] options Options
+  # @option options [String] :category The blog category, without the locale.
+  #   If not specified, blog entries retrieved will not be filtered by category.
+  # @option options [String,Symbol] :locale The locale to retrieve blog entries 
+  #   for. If none are found for this locale, those from the English blog will 
+  #   be returned instead.
+  # @option options [Boolean] :relocale if true, restore locale to links in
+  #   the entry's content with href starting "/", i.e. local absolute links
+  # @return Array<Feedzirra::Parser::AtomEntry> array of feed entries
+  # @see relocale_link()
+  # @see https://github.com/hpricot/hpricot
+  def europeana_blog_posts(options = {})
+    url = "http://europeana1914-1918.blogspot.com/feeds/posts/default"
+    url = url + "/-/"
+
+    if options[:locale].blank?
+      options[:locale] = I18n.locale
+    end
+    options[:locale] = options[:locale].to_s
+    
+    if options[:category]
+      url = url + options[:category] + '-'
+    end
+    
+    url = url + options[:locale]
+    
+    feed = Feedzirra::Feed.fetch_and_parse(url)
+    if feed.respond_to?(:entries) && feed.entries.present?
+      feed.entries.each do |entry| 
+        if options[:relocale]
+          entry.content = relocale_link(entry.content)
+        end
+        entry.content = Hpricot(entry.content)
+      end
+      feed.entries
+    elsif options[:locale] == 'en'
+      []
     else
-      url = "http://europeana1914-1918.blogspot.com/feeds/posts/default/-/#{locale.to_s}"
+      europeana_blog_posts(options.merge(:locale => 'en'))
+    end
+  end
+  
+  ##
+  # Retrieves entries from the Great War Archive blog via Atom feed
+  #
+  # Unlike {europeana_blog_posts()}, this method does not run the entries'
+  # content through Hpricot.
+  #
+  # @see europeana_blog_posts()
+  def gwa_blog_posts(options = {})
+    url = "http://thegreatwararchive.blogspot.com/feeds/posts/default"
+    url = url + "/-/"
+
+    if options[:locale].blank?
+      options[:locale] = I18n.locale
+    end
+    options[:locale] = options[:locale].to_s
+    
+    if options[:category].blank? && (options[:locale] == 'de')
+      options[:locale] = 'De'
+    end
+    url = url + options[:locale]
+    
+    if options[:category]
+      url = url + '-' + options[:category]
     end
     
     feed = Feedzirra::Feed.fetch_and_parse(url)
     if feed.respond_to?(:entries) && feed.entries.present?
-      feed.entries
-    elsif default
+      feed.entries.each do |entry| 
+        if options[:relocale]
+          entry.content = relocale_link(entry.content)
+        end
+      end
+      feed.entries.reject { |entry| entry.blank? }
+    elsif options[:locale] == 'en'
       []
     else
-      explore_by_theme
+      gwa_blog_posts(options.merge(:locale => 'en'))
     end
   end
-  
-  def explore_by_object(locale = nil)
-    if locale.nil?
-      default = true
-      url = "http://europeana1914-1918.blogspot.com/feeds/posts/default/-/explore-by-object-en"
-    else
-      url = "http://europeana1914-1918.blogspot.com/feeds/posts/default/-/#{locale.to_s}"
-    end
-    
-    feed = Feedzirra::Feed.fetch_and_parse(url)
-    if feed.respond_to?(:entries) && feed.entries.present?
-      feed.entries
-    elsif default
-      []
-    else
-      explore_by_object
-    end
-  end
-  
-  def explore_introduction(locale = nil)
-    if locale.nil?
-      default = true
-      url = "http://europeana1914-1918.blogspot.com/feeds/posts/default/-/explore-introduction-en"
-    else
-      url = "http://europeana1914-1918.blogspot.com/feeds/posts/default/-/#{locale.to_s}"
-    end
-    
-    feed = Feedzirra::Feed.fetch_and_parse(url)
-    if feed.respond_to?(:entries) && feed.entries.present?
-      feed.entries
-    elsif default
-      []
-    else
-      explore_introduction
-    end
-  end
-  
 end
