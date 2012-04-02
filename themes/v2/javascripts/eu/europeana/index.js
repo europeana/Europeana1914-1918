5ae83a691e46afaf0e9c5d6ad24dea0af6e4a6d0
dan entous <d_entous@yahoo.com>
2012-04-02 08:50:42 +0000

    adjusting theme v2

--- themes/v2/javascripts/eu/europeana/index.js
+++ themes/v2/javascripts/eu/europeana/index.js
@@ -10,8 +10,12 @@
 	});
 	
 	
-	//jQuery('#stories-from-the-archive').elastislide({
-	//	imageW : jQuery('#stories-from-the-archive div > ul').outerWidth(true)
-	//});
+	jQuery('#stories-from-the-archive').elastislide({
+		imageW : jQuery('#stories-from-the-archive div > ul').eq(0).outerWidth(true),
+		border : 0,
+		margin : 0,
+		onClick : function( $item ) { return true; }
+	});
+	
 	
 }());
