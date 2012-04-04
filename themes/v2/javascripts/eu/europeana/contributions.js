156e517ce98a5c9ade071607116f9a576cdfd4f3
dan entous <d_entous@yahoo.com>
2012-04-04 10:13:22 +0000

    adjusting the contributions show page

--- themes/v2/javascripts/eu/europeana/contributions.js
+++ themes/v2/javascripts/eu/europeana/contributions.js
@@ -1,6 +1,21 @@
 (function(){
 
 	'use strict';
-	RunCoCo.GMap.Display.init('story-map');
+	//RunCoCo.GMap.Display.init('story-map');
+	
+	jQuery('#image-viewer').elastislide({
+		imageW : jQuery('#image-viewer div > ul').eq(0).outerWidth(true) - 4,
+		border : 0,
+		margin : 0,
+		onClick : function( $item ) { return true; }
+	});
+	
+	
+	jQuery('#thumbnail-viewer').elastislide({
+		imageW : jQuery('#thumbnail-viewer').eq(0).outerWidth(true),
+		border : 0,
+		margin : 0,
+		onClick : function( $item ) { return true; }
+	});
 	
 }());
