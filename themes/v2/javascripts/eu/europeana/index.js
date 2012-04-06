d64a000e56abb7ea130b5e02f87632e1b5e78774
dan entous <d_entous@yahoo.com>
2012-04-06 09:52:43 +0000

    adding infinitescroll to index page

--- themes/v2/javascripts/eu/europeana/index.js
+++ themes/v2/javascripts/eu/europeana/index.js
@@ -10,4 +10,23 @@
 	});
 	
 	
+	jQuery('#news-articles').infinitescroll({
+		navSelector  	: '.next-article',
+		nextSelector 	: '.next-article',
+		itemSelector 	: 'table',
+		debug		 	: false,
+		dataType	 	: 'html',
+		
+		loading: {
+			finished: undefined,
+			finishedMsg: "",
+			img: "/themes/v2/images/icons/loading-animation.gif",
+			msg: null,
+			msgText: "",
+			selector: null,
+			speed: 'slow',
+			start: undefined
+		}
+	});
+	
 }());
