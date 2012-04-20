96ddc6a1d4391a448629558780f8aaf779bf2f29
richard doe <richard.doe@rwdit.net>
2012-04-20 13:04:14 +0000

    Prevent contributors from deleting attachments when contributions are in revised status.

--- app/models/permissions.rb
+++ app/models/permissions.rb
@@ -144,7 +144,7 @@ class Permissions < Aegis::Permissions
   
   action :delete_attachment do
     allow :contributor do |attachment| 
-      (attachment.contribution.contributor == user) && [ :draft, :submitted, :revised ].include?(attachment.contribution.status)
+      (attachment.contribution.contributor == user) && [ :draft, :submitted ].include?(attachment.contribution.status)
     end
     allow :guest do |attachment|
       !RunCoCo.configuration.registration_required? && (attachment.contribution.contact == user.contact) && (attachment.contribution.status == :draft)
