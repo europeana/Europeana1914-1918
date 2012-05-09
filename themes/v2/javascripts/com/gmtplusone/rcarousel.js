6e55f523e62b649fe5b00a632b1adcef40073f73
dan entous <d_entous@yahoo.com>
2012-05-09 14:34:55 +0000

    debug for swipe

--- themes/v2/javascripts/com/gmtplusone/rcarousel.js
+++ themes/v2/javascripts/com/gmtplusone/rcarousel.js
@@ -149,13 +149,13 @@
 				
 				case 'up':
 					
-					jQuery('html,body').animate({scrollTop: targetOffset.top + 50}, 700);
+					jQuery('html,body').animate({scrollTop: targetOffset.top + 30}, 1000);
 					break;
 				
 				
 				case 'down':
 					
-					jQuery('html,body').animate({ scrollTop: targetOffset.top + self.$carousel_container.height() + 20 }, 700);
+					jQuery('html,body').animate({ scrollTop: targetOffset.top + self.$carousel_container.height() + 40 }, 1000);
 					break;
 				
 				
