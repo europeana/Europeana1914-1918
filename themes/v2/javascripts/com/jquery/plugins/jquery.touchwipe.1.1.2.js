9d587f5e0e355022280284bd3bd295a585a61b84
dan entous <d_entous@yahoo.com>
2012-05-14 14:13:14 +0000

    attempting to resolve touchwipe issue with vertical scrolling over carousel

--- themes/v2/javascripts/com/jquery/plugins/jquery.touchwipe.1.1.2.js
+++ themes/v2/javascripts/com/jquery/plugins/jquery.touchwipe.1.1.2.js
@@ -7,90 +7,113 @@
  * @version 1.1 (1st September 2010) - support wipe up and wipe down
  * @version 1.0 (15th July 2010)
  */
-(function($) { 
-   $.fn.touchwipe = function(settings) {
-     var config = {
-    		min_move_x: 20,
-    		min_move_y: 20,
- 			wipeLeft: function() { },
- 			wipeRight: function() { },
- 			wipeUp: function() { },
- 			wipeDown: function() { },
-			preventDefaultEvents: {
-			   up : false,
-			   right : true,
-			   down : false,
-			   left : true
+(function() {
+	
+	'use strict';
+	
+	
+  jQuery.fn.touchwipe = function(settings) {
+		
+		var $target = this,
+				config = {
+					min_move_x: 20,
+					min_move_y: 20,
+					wipeLeft: function() { },
+					wipeRight: function() { },
+					wipeUp: function() { },
+					wipeDown: function() { }
+				};
+    
+		
+    if (settings) {
+			
+			jQuery.extend(config, settings);
+			
+		}
+		
+    $target.each(function() {
+			
+			var $elm = jQuery(this),
+					startX,
+					startY,
+					isMoving = false;
+			
+			
+			function cancelTouch() {
+				
+				$elm.off( 'touchmove' );
+				startX = null;
+				isMoving = false;
+				
 			}
-	 };
-     
-     if (settings) $.extend(config, settings);
+			
+			function onTouchMove( evt ) {
+				
+				if ( isMoving ) {
+					
+					var x = evt.touches[0].pageX,
+							y = evt.touches[0].pageY,
+							dx = startX - x,
+							dy = startY - y;
+					
+					
+					if ( Math.abs(dx) >= config.min_move_x ) {
+						
+						cancelTouch();
+						
+						if ( dx > 0 ) {
+							
+							config.wipeLeft( evt );
+							
+						} else {
+							
+							config.wipeRight( evt );
+							
+						}
+						
+					} else if ( Math.abs(dy) >= config.min_move_y ) {
+						
+						cancelTouch();
+						
+						if ( dy > 0 ) {
+							
+							config.wipeDown( evt );
+							
+						} else {
+							
+							config.wipeUp( evt );
+							
+						}
+						
+					}
+					
+				}
+				
+			}
+			
+			function onTouchStart( evt ) {
+				
+				if ( evt.touches.length === 1 ) {
+					
+					startX = evt.touches[0].pageX;
+					startY = evt.touches[0].pageY;
+					isMoving = true;
+					$elm.on( 'touchmove', onTouchMove );
+					
+				}
+				
+			}
+			
+			if ( 'undefined' !== typeof document.documentElement.ontouchstart ) {
+				
+				$elm.on( 'touchstart', onTouchStart);
+				
+			}
+			
+		});
+		
+		return this;
+		
+  };
  
-     this.each(function() {
-    	 var startX;
-    	 var startY;
-		 var isMoving = false;
-
-    	 function cancelTouch() {
-    		 this.removeEventListener('touchmove', onTouchMove);
-    		 startX = null;
-    		 isMoving = false;
-    	 }	
-    	 
-    	 function onTouchMove(e) {
-    		 if(isMoving) {
-	    		 var x = e.touches[0].pageX;
-	    		 var y = e.touches[0].pageY;
-	    		 var dx = startX - x;
-	    		 var dy = startY - y;
-	    		 if(Math.abs(dx) >= config.min_move_x) {
-	    			cancelTouch();
-	    			if(dx > 0) {
-							if(config.preventDefaultEvents.left) {
-								e.preventDefault();
-							}
-	    				config.wipeLeft();
-	    			}
-	    			else {
-							if(config.preventDefaultEvents.right) {
-								e.preventDefault();
-							}
-	    				config.wipeRight();
-	    			}
-	    		 }
-	    		 else if(Math.abs(dy) >= config.min_move_y) {
-		    			cancelTouch();
-		    			if(dy > 0) {
-								if(config.preventDefaultEvents.down) {
-									e.preventDefault();
-								}
-		    				config.wipeDown();
-		    			}
-		    			else {
-								if(config.preventDefaultEvents.up) {
-									e.preventDefault();
-								}
-		    				config.wipeUp();
-		    			}
-		    		 }
-    		 }
-    	 }
-    	 
-    	 function onTouchStart(e)
-    	 {
-    		 if (e.touches.length == 1) {
-    			 startX = e.touches[0].pageX;
-    			 startY = e.touches[0].pageY;
-    			 isMoving = true;
-    			 this.addEventListener('touchmove', onTouchMove, false);
-    		 }
-    	 }    	 
-    	 if ('ontouchstart' in document.documentElement) {
-    		 this.addEventListener('touchstart', onTouchStart, false);
-    	 }
-     });
- 
-     return this;
-   };
- 
- })(jQuery);
+}());
