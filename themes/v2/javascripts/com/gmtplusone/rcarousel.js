e295219449921e9d03c13a955f922997c81bd004
dan entous <d_entous@yahoo.com>
2012-05-14 23:07:15 +0000

    attempting to resolve touchwipe issue with vertical scrolling over carousel

--- themes/v2/javascripts/com/gmtplusone/rcarousel.js
+++ themes/v2/javascripts/com/gmtplusone/rcarousel.js
@@ -175,23 +175,16 @@
 			// http://www.netcu.de/jquery-touchwipe-iphone-ipad-library
 			if ( jQuery().touchwipe ) {
 				
-				self.$carousel_container.touchwipe({
-					wipeLeft : function( evt ) { evt.preventDefault(); self.$next.trigger('click'); },
-					wipeRight : function( evt ) { evt.preventDefault(); self.$prev.trigger('click'); },
-					wipeUp : function( evt ) {},
-					wipeDown : function( evt ) {}
+				self.$items.each(function() {
+					
+					jQuery(this).touchwipe({
+						wipeLeft : function( evt ) { evt.preventDefault(); self.$next.trigger('click'); },
+						wipeRight : function( evt ) { evt.preventDefault(); self.$prev.trigger('click'); },
+						wipeUp : function( evt ) {},
+						wipeDown : function( evt ) {}
+					});
+					
 				});
-				
-				//self.$items.each(function() {
-				//	
-				//	jQuery(this).touchwipe({
-				//		wipeLeft : function( evt ) { evt.preventDefault(); self.$next.trigger('click'); },
-				//		wipeRight : function( evt ) { evt.preventDefault(); self.$prev.trigger('click'); },
-				//		wipeUp : function( evt ) {},
-				//		wipeDown : function( evt ) {}
-				//	});
-				//	
-				//});
 				
 			}
 			
