41c95723d00574122e00ddf56b9112f87ceffa10
dan entous <d_entous@yahoo.com>
2012-05-16 05:59:54 +0000

    adjusting mobile menu

--- themes/v2/javascripts/eu/europeana/navigation-mobile.js
+++ themes/v2/javascripts/eu/europeana/navigation-mobile.js
@@ -69,7 +69,7 @@
 	$navigation_mobile.add(document).on('click', handleClick);
 	
 	if ( $navigation_user_menu.length === 1 ) {
-		$navigation_main.css('top','181px');
+		$navigation_main.css('top','194px');
 	}
 	
 	jQuery(window).on('resize', function() {
