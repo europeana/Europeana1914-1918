d66be498accb742e6b32ef5b135539de9981037d
richard doe <richard.doe@rwdit.net>
2012-05-24 10:14:29 +0000

    Remove eager loading of contribution associations in collection search.

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -165,9 +165,9 @@ class ContributionsController < ApplicationController
     current_user.may_search_contributions!
     @query = params[:q]
     # Minimal eager loading of associations if search result partials are not pre-cached.
-    search_options = { :page => params[:page], :per_page => (params[:count] || 48), :include => [ :attachments, :metadata ] }
+#    search_options = { :page => params[:page], :per_page => (params[:count] || 48), :include => [ :attachments, :metadata ] }
     # No eager loading if partials are all pre-cached.
-#    search_options = { :page => params[:page], :per_page => (params[:count] || 48) }
+    search_options = { :page => params[:page], :per_page => (params[:count] || 48) }
     @contributions = search_contributions(:published, @query, search_options)
   end
   
@@ -179,9 +179,9 @@ class ContributionsController < ApplicationController
     field = MetadataField.find_by_name!(params[:field_name])
     if taxonomy_term = field.taxonomy_terms.find_by_term(@term)
       # Minimal eager loading of associations if search result partials are not pre-cached.
-      search_options = { :taxonomy_term => taxonomy_term, :page => params[:page], :per_page => (params[:count] || 48), :include => [ :attachments, :metadata ] }
+#      search_options = { :taxonomy_term => taxonomy_term, :page => params[:page], :per_page => (params[:count] || 48), :include => [ :attachments, :metadata ] }
       # No eager loading if partials are all pre-cached.
-#      search_options = { :taxonomy_term => taxonomy_term, :page => params[:page], :per_page => (params[:count] || 48) }
+      search_options = { :taxonomy_term => taxonomy_term, :page => params[:page], :per_page => (params[:count] || 48) }
       @contributions = search_contributions(:published, nil, search_options)
     else
       @contributions = []
