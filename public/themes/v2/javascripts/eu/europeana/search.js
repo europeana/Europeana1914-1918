43fbfc9cdb5d3f2502884a9d3b9df6c4a0eced63
dan entous <d_entous@yahoo.com>
2012-09-06 16:49:14 +0000

    story 112 - https://sites.google.com/site/projecteuropeana19141918/backlog/acceptance-criteria/story-112
    * added in a "styled" tabbed look and feel
    * added masonry support for clicks between tabs

--- public/themes/v2/javascripts/eu/europeana/search.js
+++ public/themes/v2/javascripts/eu/europeana/search.js
@@ -1,11 +1,184 @@
-(function( undefined ) {
+(function() {
 	
 	'use strict';
 	
 	
+	var resultTabs = {
+		
+		$tabs : jQuery('#results-tabs a'),
+		loading_feedback : '<div class="loading-feedback"></div>',
+		ajax_load_processed : true,
+		
+		
+		handleContentLoad : function( active_tab_id ) {
+			
+			this.ajax_load_processed = true;
+			jQuery( '#' + active_tab_id ).attr('data-loaded','true');
+			
+			jQuery('.stories').imagesLoaded(function() {
+				jQuery('.stories').masonry({
+					itemSelector : 'li',
+					columnWidth : 1,
+					isFitWidth : true,
+					isAnimated : true
+				});
+			});
+			
+		},
+		
+		
+		retrieveContent : function( active_tab_id ) {
+			
+			var self = this,
+					url = jQuery('#' + active_tab_id ).attr('data-url');
+			
+			
+			if ( !url || !self.ajax_load_processed || jQuery('#' + active_tab_id ).attr('data-loaded') === 'true' ) { return; }
+			self.ajax_load_processed = false;
+			
+			try {
+				
+				jQuery(jQuery('#' + active_tab_id ).attr('href')).load(
+					url + ' ' + jQuery('#' + active_tab_id ).attr('href'),
+					null,
+					function() { self.handleContentLoad( active_tab_id ); }				
+				);
+				
+			} catch(e) {}
+			
+		},
+		
+		
+		toggleLoaderDiv : function( active_tab_id ) {
+			
+			this.$tabs.each(function() {
+				
+				var $elm = jQuery(this);
+				
+				if ( active_tab_id !== $elm.attr('id') ) {
+					
+					jQuery( $elm.attr('href') ).fadeOut();
+					
+				} else {
+					
+					jQuery( $elm.attr('href') ).fadeIn();
+					
+				}
+				
+			});
+			
+			jQuery( jQuery('#' + active_tab_id).attr('href') ).find('.loading-feedback').fadeIn();
+			
+				jQuery('.stories').masonry({
+					itemSelector : 'li',
+					columnWidth : 1,
+					isFitWidth : true,
+					isAnimated : true
+				});
+			
+		},
+		
+		
+		toggleTabs : function( active_tab_id ) {
+			
+			this.$tabs.each(function(){
+				
+				var $elm = jQuery(this);
+				
+				if ( $elm.attr('id') !== active_tab_id ) {
+					
+					$elm.removeClass('active');
+					
+				} else {
+					
+					$elm.addClass('active');
+					
+				}
+				
+			});
+			
+		},
+		
+		
+		handleResultsTabClick : function( evt ) {
+			
+			var self = evt.data.self,
+					$elm = jQuery(this),
+					active_tab_id = $elm.attr('id');
+			
+			evt.preventDefault();
+			self.toggleTabs( active_tab_id );
+			self.toggleLoaderDiv( active_tab_id );
+			self.retrieveContent( active_tab_id );
+			
+		},
+		
+		
+		addLoadingFeedback : function() {
+			
+			var self = this;
+			
+			self.$tabs.each(function() {
+				
+				var $elm = jQuery(this);
+				
+				if ( jQuery( $elm.attr('href') ).html() === '' ) {
+					
+					jQuery( $elm.attr('href') ).append( self.loading_feedback );
+					
+				}
+				
+			});
+			
+		},
+		
+		
+		adjustTabLinks : function() {
+			
+			this.$tabs.each(function() {
+				
+				var $elm = jQuery(this);
+				
+				if ( $elm.attr('href').substring(0,1) !== '#' ) {
+					
+					$elm.attr( 'data-url', $elm.attr('href') );
+					$elm.attr( 'href', '#' + $elm.attr('id').replace('-tab','') );
+					$elm.attr( 'data-loaded', 'false' );
+					
+				} else {
+					
+					$elm.attr( 'data-loaded', 'true' );
+					$elm.addClass('active');
+					
+				}
+				
+				
+			});
+			
+		},
+		
+		
+		setupTabs : function() {
+			
+			this.adjustTabLinks();
+			this.addLoadingFeedback();
+			jQuery('#results-tab-1914,#results-tab-europeana').on( 'click', {self:this}, this.handleResultsTabClick );			
+			
+		},
+		
+		
+		init : function() {
+			
+			this.setupTabs();
+			
+		}
+		
+	};
+	
+	
 	function init() {
 		
-		var $container = jQuery('#stories');
+		var $container = jQuery('.stories');
 		
 		$container.imagesLoaded(function() {
 			$container.masonry({
@@ -22,18 +195,8 @@
 			select: function(event, ui) { var self = this; setTimeout( function() { jQuery(self).closest('form').submit(); }, 100 ); }
 		});
 		
-		jQuery('#results-tabs').tabs({ 
-		  ajaxOptions: { data: 'layout=0' }, 
-		  selected: RunCoCo.uiTabSelected,
-		  select: function(event, ui) { 
-		    if (ui.index == 1) {
-		      var searchFormAction = RunCoCo.relativeUrlRoot + '/' + I18n.currentLocale() + '/europeana/search';
-		    } else {
-		      var searchFormAction = RunCoCo.relativeUrlRoot + '/' + I18n.currentLocale() + '/search';
-		    }
-		    jQuery('#search').attr('action', searchFormAction);
-	    },
-	  });
+		resultTabs.init();
+		
 	}
 	
 	init();
