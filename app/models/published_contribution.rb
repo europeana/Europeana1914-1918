9a8ba2b618dc5da1613ba7e6dd3e37ecae02a8a4
richard doe <richard.doe@rwdit.net>
2012-12-14 13:38:53 +0000

    Fix DC namespace in OAI DC metadata format.

--- app/models/published_contribution.rb
+++ app/models/published_contribution.rb
@@ -11,54 +11,54 @@ class PublishedContribution < Contribution
   #
   def to_oai_dc
     xml = Builder::XmlMarkup.new
-    xml.tag!("oai_dc:dc",
+    xml.tag!('oai_dc:dc'
      OAI::Provider::Metadata::DublinCore.instance.header_specification.merge(
       { 'xmlns:dcterms' => "http://purl.org/dc/terms/" }
      )
     ) do
-      xml.tag!('oai_dc:title', title)
+      xml.tag!('dc:title', title)
       unless metadata.fields['description'].blank?
-        xml.tag!('oai_dc:description', metadata.fields['description'])
+        xml.tag!('dc:description', metadata.fields['description'])
       end
       if metadata.fields['contributor_behalf'].present?
-        xml.tag!('oai_dc:contributor', metadata.fields['contributor_behalf'])
+        xml.tag!('dc:contributor', metadata.fields['contributor_behalf'])
       else
-        xml.tag!('oai_dc:contributor', contributor.contact.full_name)
+        xml.tag!('dc:contributor', contributor.contact.full_name)
       end
       unless metadata.fields['lang'].blank?
         metadata.fields['lang'].each do |lang|
-          xml.tag!('oai_dc:language', lang)
+          xml.tag!('dc:language', lang)
         end
       end
       unless metadata.fields['lang_other'].blank?
-        xml.tag!('oai_dc:language', metadata.fields['lang_other'])
+        xml.tag!('dc:language', metadata.fields['lang_other'])
       end
       unless metadata.fields['keywords'].blank?
         metadata.fields['keywords'].each do |keyword|
-          xml.tag!('oai_dc:subject', keyword)
+          xml.tag!('dc:subject', keyword)
         end
       end
       unless metadata.fields['theatres'].blank?
         metadata.fields['theatres'].each do |theatre|
-          xml.tag!('oai_dc:subject', theatre)
+          xml.tag!('dc:subject', theatre)
         end
       end
       unless metadata.fields['date_from'].blank? && metadata.fields['date_to'].blank?
         period = 'scheme=W3C-DTF;'
         period << (' start=' + metadata.fields['date_from'] + ';') unless metadata.fields['date_from'].blank?
         period << (' end=' + metadata.fields['date_to'] + ';') unless metadata.fields['date_to'].blank?
-        xml.tag!('oai_dc:date', period, :'xsi:type' => "dcterms:Period")
+        xml.tag!('dc:date', period, :'xsi:type' => "dcterms:Period")
       end
       unless (host = Rails.application.config.action_mailer.default_url_options[:host]).blank?
         port = Rails.application.config.action_mailer.default_url_options[:port]
         Rails.application.routes.default_url_options[:locale] ||= Rails.configuration.i18n.default_locale
         identifier = localeless_contribution_url(:host => host, :port => port, :id => self.id)
-        xml.tag!('oai_dc:identifier', identifier, :'xsi:type' => "dcterms:URI")
+        xml.tag!('dc:identifier', identifier, :'xsi:type' => "dcterms:URI")
       end
-      xml.tag!('oai_dc:type', 'Text', :'xsi:type' => "dcterms:DCMIType")
+      xml.tag!('dc:type', 'Text', :'xsi:type' => "dcterms:DCMIType")
       [ 'location_placename', 'location_map' ].each do |coverage|
         unless metadata.fields[coverage].blank?
-          xml.tag!('oai_dc:coverage', metadata.fields[coverage])
+          xml.tag!('dc:coverage', metadata.fields[coverage])
         end
       end
     end
