c44b0dd6a475b0e727f3885d94649d807da22338
andy j maclean <andyjmaclean@gmail.com>
2013-05-27 09:55:26 +0000

    Search init added to utils

--- public/themes/v3/javascripts/js/utils.js
+++ public/themes/v3/javascripts/js/utils.js
@@ -11,6 +11,25 @@
 			$elm.css( 'background-color', start_color );
 			setTimeout( function() { $elm.css('background-color', end_color ); }, duration );
 			
+		},
+		
+		initSearch : function(){
+			//$('.submit-cell').css("width",	$('.submit-cell button')	.outerWidth(true) + "px"); 
+			//$('.menu-cell').css("width",	$('#search-menu')				.outerWidth(true) + "px");
+			
+			$('.submit-cell button').css("border-left",	"solid 1px #4C7ECF");	// do this after the resize to stop 1px gap in FF
+			
+			$("table.t-query").css("display",		"none");
+			$("table.t-query").css("visibility",	"visible");
+			$("table.t-query").fadeIn(600);
+			
+			$('#q').focus(function(){
+				$('.query-cell').addClass('glow');
+			});
+			
+			$('#q').blur(function(){
+				$('.query-cell').removeClass('glow');
+			});
 		}
 		
 	};
