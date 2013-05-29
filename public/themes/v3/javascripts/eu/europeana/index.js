2892cdfdbbbe049e07a347a7c2b77ed9b3708a61
andy j maclean <andyjmaclean@gmail.com>
2013-05-29 15:29:44 +0000

    Index page styling

--- public/themes/v3/javascripts/eu/europeana/index.js
+++ public/themes/v3/javascripts/eu/europeana/index.js
@@ -3,10 +3,44 @@
 	'use strict';
 	
 	
-	jQuery('#index-featured').imagesLoaded(function() {
+	$('#index-featured').imagesLoaded(function() {
 		this.rCarousel();
 	});	
 	
-	jQuery('#news-articles').readMore({ read_more_link : '#read-more' });
+	$('#news-articles').readMore({ read_more_link : '#read-more' });
+
+	
+	$('#featured-categories').imagesLoaded(function() {
+		
+		$(this).masonry({
+			itemSelector : 'li',
+			columnWidth : function( containerWidth ) {
+				
+				var winWidth = $('html').width();
+
+				if(winWidth < 500){
+					$('#featured-categories ul').css('width', $('#featured-categories ul img:first').width() + 'px');
+					
+					return parseInt(containerWidth) - parseInt((2 * 8) );			// (container-w / noCols) - (( (noCols -1 + 2 (edges)) * gutterWidth ) / noCols)
+				}
+				else{
+					$('#featured-categories ul').css('width', 'auto');
+				}
+				if(winWidth < 1130){
+					return parseInt(containerWidth / 2) - parseInt((3 * 8) / 2);	// (container-w / noCols) - (( (noCols -1 + 2 (edges)) * gutterWidth ) / noCols)
+				}
+				else{
+					return parseInt(containerWidth / 3) - parseInt((4 * 8) / 3);	// (container-w / noCols) - (( (noCols -1 + 2 (edges)) * gutterWidth ) / noCols)					
+				}
+					
+			},
+			gutterWidth:	8,
+			isAnimated :	true
+		});		
+
+	});	
+
+	
+	js.utils.initSearch();
 	
 }());
