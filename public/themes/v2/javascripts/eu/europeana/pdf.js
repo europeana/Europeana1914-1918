8d0bb27a35e6efd6bf096630fe3e74de52e6aa98
dan entous <d_entous@yahoo.com>
2013-06-28 08:01:00 +0000

    adding in a view based on the viewer at http://mozilla.github.io/pdf.js/web/viewer.html

--- public/themes/v2/javascripts/eu/europeana/pdf.js
+++ public/themes/v2/javascripts/eu/europeana/pdf.js
@@ -9,15 +9,18 @@
 
 		url : RunCoCo.pdf_url,
 		pdfDoc : null,
+		checkForPdfDoc : 0,
 		pageNum : 1,
 		scale : 0.8,
-		canvas : document.getElementById('the-canvas'),
+		canvas : document.getElementsByTagName('canvas')[0],
 		ctx : null,
 
-		//
-		// Get page info from document, resize canvas accordingly, and render page
-		//
+
+		/*
+		 * Get page info from document, resize canvas accordingly, and render page
+		 */
 		renderPage : function( num ) {
+
 			if ( null === pdf.pdfDoc ) {
 				return;
 			}
@@ -37,26 +40,24 @@
 			});
 
 			// Update page counters
-			document.getElementById('page_num').textContent = pdf.pageNum;
-			document.getElementById('page_count').textContent = pdf.pdfDoc.numPages;
+			//document.getElementById('page_num').textContent = pdf.pageNum;
+			//document.getElementById('page_count').textContent = pdf.pdfDoc.numPages;
 
 		},
 
-		//
-		// Go to previous page
-		//
+
 		goPrevious : function() {
+
 			if ( pdf.pageNum <= 1 ) {
 				return;
 			}
 
 			pdf.pageNum -= 1;
 			pdf.renderPage( pdf.pageNum );
+
 		},
 
-		//
-		// Go to next page
-		//
+
 		goNext : function() {
 			if ( pdf.pageNum >= pdf.numPages ) {
 				return;
@@ -66,27 +67,78 @@
 			pdf.renderPage( pdf.pageNum );
 		},
 
+
 		fetchPdf : function() {
-			//
-			// Asynchronously download PDF as an ArrayBuffer
-			//
+
 			PDFJS.getDocument( pdf.url ).then(
 				function getPdfHelloWorld( _pdfDoc ) {
 					pdf.pdfDoc = _pdfDoc;
 					pdf.renderPage( pdf.pageNum );
 				}
 			);
+
 		},
 
+
+		placeControls : function() {
+//<span><%= t('formtastic.labels.attachment.metadata.page_number') %> :
+// <span id="page_num"></span> / <span id="page_count"></span></span>
+			var page_num = document.createElement('span'),
+				page_count = document.createElement('span'),
+				prev = document.createElement('button'),
+				next = document.createElement('button');
+
+			page_num.id = 'page_num';
+			page_count.id = 'page_count';
+
+			prev.id = 'prev';
+			prev.textContent = I18n.t('javascripts.previous');
+
+			next.id = 'next';
+			next.textContent = I18n.t('javascripts.next');
+
+			lib.addEvent( prev, 'click', pdf.goPrevious );
+			lib.addEvent( next, 'click', pdf.goNext );
+
+			pdf.canvas.parentNode
+			//pdf.canvas.insertBefore( page_num, pdf.canvas.parentNode );
+			//pdf.canvas.insertBefore( page_count, pdf.canvas.parentNode );
+			//document.getElementById('controls').appendChild( next );
+			//document.getElementById('controls').appendChild( prev );
+
+		},
+
+
+		pdfSetup : function() {
+
+			if ( null === pdf.pdfDoc ) {
+				pdf.checkForPdfDoc += 1;
+
+				if ( pdf.checkForPdfDoc < 20 ) {
+					setTimeout( function() { pdf.pdfSetup(); }, 200 );
+				} else {
+					console.log( 'pdfDoc was not available before throttle limit' );
+				}
+
+				return;
+			}
+
+			document.body.style.width = pdf.canvas.width + 'px';
+
+		},
+
+
 		init : function() {
+
 			pdf.ctx = pdf.canvas.getContext('2d');
-			lib.addEvent( document.getElementById('prev'), 'click', pdf.goPrevious );
-			lib.addEvent( document.getElementById('next'), 'click', pdf.goNext );
 			pdf.fetchPdf();
+			pdf.placeControls();
+			pdf.pdfSetup();
+
 		}
 
 	};
 
-	pdf.init();
+	setTimeout( function() { pdf.init(); }, 1000 );
 
 }());
