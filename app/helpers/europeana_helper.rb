21e4cad2ef96f60f626667027cb3a3f7ef924acd
richard doe <richard.doe@rwdit.net>
2013-07-02 09:13:39 +0000

    Ticket #19: Display Vimeo videos from institutional content via oEmbed.

--- app/helpers/europeana_helper.rb
+++ app/helpers/europeana_helper.rb
@@ -1,4 +1,6 @@
-module EuropeanaHelper  
+require 'digest/md5'
+
+module EuropeanaHelper
   def nav_links
     links = []
     links << [ t('layout.navigation.contribute'), contributor_dashboard_path ]
@@ -18,5 +20,30 @@ module EuropeanaHelper
   def europeana_record_url(id)
     dataset_id, record_id = id[1..-1].split('/')
     show_europeana_url(:dataset_id => dataset_id, :record_id => record_id)
+  end
+  
+  ##
+  # Gets the oEmbed HTML from an EDM object's edm:isShownBy aggregation
+  #
+  # Responses from oEmbed providers are cached for 1 day.
+  #
+  # @param [Hash] Representation of an EDM object
+  # @return [String] HTML for the oEmbed resource
+  #
+  def oembed_html(edmObject)
+    if edmObject['aggregations'] && url = edmObject['aggregations'].first['edmIsShownBy']
+    
+      cache_key = "oembed/response/" + Digest::MD5.hexdigest(url)
+      if controller.fragment_exist?(cache_key)
+        resource_fields = YAML::load(controller.read_fragment(cache_key))
+      elsif provider = OEmbed::Providers.find(url)
+        resource = provider.get(url)
+        resource_fields = resource.fields
+        controller.write_fragment(cache_key, resource_fields.to_yaml, :expires_in => 1.day)
+      end
+    
+      return resource_fields['html'] unless resource_fields.nil?
+    end
+    ''
   end
 end
