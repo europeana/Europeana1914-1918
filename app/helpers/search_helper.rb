0794af5fdaa5127c6de212c6006603caf9210a6a
richard doe <richard.doe@rwdit.net>
2013-07-04 12:15:53 +0000

    Ticket #72: Reduce number of database queries made for facet display.

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -7,4 +7,24 @@ module SearchHelper
       facet_name
     end
   end
+  
+  def link_to_facet_row(facet_name, row_value)
+    @@metadata_fields ||= {}
+    
+    if row_value.is_a?(Integer)
+      if taxonomy_field_facet = facet_name.to_s.match(/^metadata_(.+)_ids$/)
+        field_name = taxonomy_field_facet[1]
+        unless @@metadata_fields[field_name]
+          @@metadata_fields[field_name] = MetadataField.includes(:taxonomy_terms).find_by_name(field_name)
+        end
+        if row_term = @@metadata_fields[field_name].taxonomy_terms.select { |term| term.id == row_value }.first
+          row_label = row_term.term
+        end
+      end
+    end
+    
+    row_label ||= row_value.to_s
+    
+    link_to row_label, request.query_parameters.merge(:page => 1, :facets => (request.query_parameters[:facets] || {}).merge({ facet_name => row_value }))
+  end
 end
