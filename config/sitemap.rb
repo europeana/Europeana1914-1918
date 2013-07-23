eec28bde0683d96f3d945405a9e751c35e727640
Richard Doe <richard.doe@rwdit.net>
2013-07-23 17:22:06 +0100

    Ticket #87: Omit attachment pages from sitemap.

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -5,7 +5,7 @@ SitemapGenerator::Sitemap.create do
   # The root path '/' and sitemap index file are added automatically for you.
   # Links are added to the Sitemap in the order they are specified.
   
-  # Published contributions, their attachments, and their image files
+  # Published contributions, and their image files
   Contribution.published.find_each do |contribution|
     attachments = contribution.attachments.includes(:metadata => :field_license_terms)
     
@@ -21,12 +21,6 @@ SitemapGenerator::Sitemap.create do
   
     I18n.available_locales.each do |locale|
       add contribution_path(:locale => locale, :id => contribution.id), :lastmod => contribution.updated_at, :images => images
-    end
-    
-    attachments.each do |attachment|
-      I18n.available_locales.each do |locale|
-        add contribution_attachment_path(:locale => locale, :contribution_id => contribution.id, :id => attachment.id), :lastmod => attachment.updated_at
-      end
     end
   end
 end
