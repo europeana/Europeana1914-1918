11312ef7ca1b46c7c5a2db3ba126a76594f00ccf
Richard Doe <richard.doe@rwdit.net>
2013-07-23 12:40:56 +0100

    Ticket #87: Associate images with contributions in sitemap.

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,30 +2,30 @@
 SitemapGenerator::Sitemap.default_host = RunCoCo.configuration.site_url
 
 SitemapGenerator::Sitemap.create do
-  # Put links creation logic here.
-  #
   # The root path '/' and sitemap index file are added automatically for you.
   # Links are added to the Sitemap in the order they are specified.
   
   # Published contributions, their attachments, and their image files
   Contribution.published.find_each do |contribution|
+    attachments = contribution.attachments.includes(:metadata => :field_license_terms)
+    
+    images = attachments.select(&:image?).collect do |attachment|
+      { 
+        :loc => attachment.file.url(:original, :timestamp => false), 
+        :title => attachment.title.blank? ? nil : attachment.title, 
+        :caption => attachment.metadata.field_description.blank? ? nil : attachment.metadata.field_description, 
+        :license => attachment.metadata.field_license_terms.collect(&:term).first,
+        :geo_location => attachment.metadata.field_location_placename.blank? ? nil : attachment.metadata.field_location_placename
+      }
+    end
+  
     I18n.available_locales.each do |locale|
-      add contribution_path(:locale => locale, :id => contribution.id), :lastmod => contribution.updated_at
+      add contribution_path(:locale => locale, :id => contribution.id), :lastmod => contribution.updated_at, :images => images
     end
     
-    contribution.attachments.includes(:metadata => :field_license_terms).each do |attachment|
-      images = []
-      if attachment.image?
-        images = [ { 
-          :loc => attachment.file.url(:original, :timestamp => false), 
-          :title => attachment.title, 
-          :caption => attachment.metadata.field_description, 
-          :license => attachment.metadata.field_license_terms.collect(&:term).first,
-          :geo_location => attachment.metadata.field_location_placename
-        } ]
-      end
+    attachments.each do |attachment|
       I18n.available_locales.each do |locale|
-        add contribution_attachment_path(:locale => locale, :contribution_id => contribution.id, :id => attachment.id), :lastmod => attachment.updated_at, :images => images
+        add contribution_attachment_path(:locale => locale, :contribution_id => contribution.id, :id => attachment.id), :lastmod => attachment.updated_at
       end
     end
   end
