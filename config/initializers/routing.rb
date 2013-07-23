098d140330bb1f725e5ee3772dd75a9457dd4921
Richard Doe <richard.doe@rwdit.net>
2013-07-23 15:33:12 +0100

    Fix misnamed protocol option for default_url_options

--- config/initializers/routing.rb
+++ config/initializers/routing.rb
@@ -1,6 +1,6 @@
 # Set default options for URLs from site URL config setting entered at
 # /admin/config/edit.
 site_url = URI.parse(RunCoCo.configuration.site_url)
-default_url_options = { :locale => I18n.locale, :host => site_url.host, :port => site_url.port, :scheme => site_url.scheme }
+default_url_options = { :locale => I18n.locale, :host => site_url.host, :port => site_url.port, :protocol => site_url.scheme }
 Rails.application.routes.default_url_options.merge!(default_url_options)
-( Rails.application.config.action_mailer.default_url_options ||= {} ).merge!( :locale => I18n.locale, :host => site_url.host, :port => site_url.port, :scheme => site_url.scheme )
+( Rails.application.config.action_mailer.default_url_options ||= {} ).merge!( :locale => I18n.locale, :host => site_url.host, :port => site_url.port, :protocol => site_url.scheme )
