3813626e172c6d04a6646793f7f832e93a32af99
Richard Doe <richard.doe@rwdit.net>
2013-07-24 16:23:53 +0100

    Ticket #17: Add EDM classes to RDF vocabulary.

--- app/models/rdf/edm.rb
+++ app/models/rdf/edm.rb
@@ -4,6 +4,7 @@ module RDF
   #
   # @see http://pro.europeana.eu/edm-documentation
   class EDM < Vocabulary("http://www.europeana.eu/schemas/edm/")
+    property :Agent
     property :aggregatedCHO
     property :begin
     property :collectionName
@@ -11,12 +12,16 @@ module RDF
     property :currentLocation
     property :dataProvider
     property :end
+    property :EuropeanaAggregation
+    property :EuropeanaObject
     property :europeanaProxy
+    property :Event
     property :happenedAt
     property :hasMet
     property :hasType
     property :hasView
     property :incorporates
+    property :InformationResource
     property :isAnnotationOf
     property :isDerivativeOf
     property :isNextInSequence
@@ -28,18 +33,24 @@ module RDF
     property :isSuccessorOf
     property :landingPage
     property :language
+    property :NonInformationResource
     property :object
     property :occurredAt
+    property :PhysicalThing
+    property :Place
     property :preview
+    property :ProvidedCHO
     property :provider
     property :realizes
     property :rights
+    property :TimeSpan
     property :type
     property :ugc
     property :unstored
     property :uri
     property :userTag
     property :wasPresentAt
+    property :WebResource
     property :year
   end
 end
