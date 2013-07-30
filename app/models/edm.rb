fdb956d0b46740f9aa573b1979ffb8afaa266f67
Richard Doe <richard.doe@rwdit.net>
2013-07-30 11:40:05 +0100

    Add EDM module comment.

--- app/models/edm.rb
+++ app/models/edm.rb
@@ -1,6 +1,9 @@
+##
+# Module containing methods for conversion of application models to EDM.
+#
 module EDM
-  autoload :Story, 'edm/story'
   autoload :Item,  'edm/item'
+  autoload :Story, 'edm/story'
   
   ##
   # Renders the object as RDF N-Triples
