499e24ef2dfc237ab9176fe8df4abcb4ff6ad60a
Richard Doe <richard.doe@rwdit.net>
2013-08-15 13:04:48 +0100

    Add Open Annotation RDF vocabulary.

--- lib/runcoco/rdf.rb
+++ lib/runcoco/rdf.rb
@@ -1,5 +1,6 @@
 module RDF
   autoload :DCMIType, 'runcoco/rdf/dcmi_type'
   autoload :EDM,      'runcoco/rdf/edm'
+  autoload :OA,       'runcoco/rdf/oa'
   autoload :ORE,      'runcoco/rdf/ore'
 end
