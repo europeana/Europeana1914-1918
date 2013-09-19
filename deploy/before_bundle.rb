ff84c640e9078e11b622798e17eda674f06f5fc1
Richard Doe <richard.doe@rwdit.net>
2013-09-19 15:41:13 +0100

    SCP individual config files to utility instances.

--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -1,9 +1,19 @@
+require 'fileutils'
+
+# Copy config files from app master to utility instances
 if config.current_role == :util
   [
-    "config/environments",
-    "config/initializers",
+    "config/environments/production.rb",
+    "config/initializers/action_mailer.rb",
+    "config/initializers/devise.rb",
+    "config/initializers/europeana.rb",
+    "config/initializers/paperclip.rb",
+    "config/initializers/secret_token.rb",
     "config/s3.yml"
-  ].each do |path|
-    run "rysnc -av -e \"ssh -o StrictHostKeyChecking=no -i /home/#{config.node[:owner_name]}/.ssh/internal\" #{config.node[:owner_name]}@#{config.node[:master_app_server][:private_dns_name]}:#{shared_path}/#{path} #{shared_path}/#{path}"
+  ].each do |config_filename|
+    config_path = "#{shared_path}/#{config_filename}"
+    config_dir = File.dirname(config_path)
+    FileUtils.mkdir(config_dir) unless File.exists?(config_dir)
+    run "scp -o StrictHostKeyChecking=no -i /home/#{config.node[:owner_name]}/.ssh/internal #{config.node[:owner_name]}@#{config.node[:master_app_server][:private_dns_name]}:#{config_path} #{config_path}"
   end
 end
