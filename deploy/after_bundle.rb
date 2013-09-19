36cec91a65fa20ff82c44fcbafd190abdb7b6088
Richard Doe <richard.doe@rwdit.net>
2013-09-19 15:58:00 +0100

    Tweak EY exports dir config.

--- deploy/after_bundle.rb
+++ deploy/after_bundle.rb
@@ -1,3 +1,4 @@
-# Create app directories
-run "mkdir -p #{shared_path}/private/exports"
-run "ln -nfs #{shared_path}/private/exports #{release_path}/private/exports"
+# Directory for XML exports
+run "mkdir #{shared_path}/exports"
+run "mkdir #{release_path}/private"
+run "ln -nfs #{shared_path}/exports #{release_path}/private/exports"
