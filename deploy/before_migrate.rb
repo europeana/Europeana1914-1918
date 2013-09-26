b186383b9c07c3d43207d1dca9527db88fcaaa38
Richard Doe <richard.doe@rwdit.net>
2013-09-26 10:48:19 +0100

    Version control production environment config.

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,6 +1,3 @@
-# Rails environment
-run "ln -nfs #{shared_path}/config/environments/production.rb #{release_path}/config/environments/production.rb"
-
 # Sphinx config
 run "ln -nfs #{shared_path}/config/sphinx #{release_path}/config/sphinx" 
 run "ln -nfs #{shared_path}/config/sphinx.yml #{release_path}/config/sphinx.yml"
