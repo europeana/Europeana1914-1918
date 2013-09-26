e6a3938dbdae95f8454770df8013b6a0c688b521
Richard Doe <richard.doe@rwdit.net>
2013-09-26 13:36:34 +0100

    Add deploy hook to restart Monit workers.

--- deploy/after_symlink.rb
+++ deploy/after_symlink.rb
@@ -1,3 +1,7 @@
 # Regenerate i18n JS assets
 run "cd #{release_path}"
 run "bundle exec rake i18n:js:export"
+
+# Restart monit workers
+run 'sudo "monit -g dj_europeana19141918 restart all"'
+run 'sudo "monit -g sphinx_europeana19141918 restart all"'
