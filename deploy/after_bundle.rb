9e6eb8f7ee83b03475e4873ed8c6d54a4c0d50d0
Richard Doe <richard.doe@rwdit.net>
2013-10-08 11:38:10 +0100

    Do not create export directory in EY deploy hooks.

--- deploy/after_bundle.rb
+++
@@ -1,4 +0,0 @@
-# Directory for XML exports
-run "mkdir #{config.shared_path}/exports"
-run "mkdir #{config.release_path}/private"
-run "ln -nfs #{config.shared_path}/exports #{config.release_path}/private/exports"
