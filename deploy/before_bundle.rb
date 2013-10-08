687e659faff6c2e65dce4997ad6f0f81c3850297
Richard Doe <richard.doe@rwdit.net>
2013-10-08 14:27:26 +0100

    Copy SSMTP config file to utility instances.

--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -3,14 +3,14 @@ require 'fileutils'
 # Copy config files from app master to utility instances
 if config.current_role == :util
   [
-    "config/initializers/action_mailer.rb",
-    "config/initializers/devise.rb",
-    "config/initializers/europeana.rb",
-    "config/initializers/paperclip.rb",
-    "config/initializers/secret_token.rb",
-    "config/s3.yml"
-  ].each do |config_filename|
-    config_path = "#{config.shared_path}/#{config_filename}"
+    "/data/ssmtp/ssmtp.conf",
+    "#{config.shared_path}/config/initializers/action_mailer.rb",
+    "#{config.shared_path}/config/initializers/devise.rb",
+    "#{config.shared_path}/config/initializers/europeana.rb",
+    "#{config.shared_path}/config/initializers/paperclip.rb",
+    "#{config.shared_path}/config/initializers/secret_token.rb",
+    "#{config.shared_path}/config/s3.yml"
+  ].each do |config_path|
     config_dir = File.dirname(config_path)
     FileUtils.mkdir(config_dir) unless File.exists?(config_dir)
     run "scp -o StrictHostKeyChecking=no -i /home/#{config.node[:owner_name]}/.ssh/internal #{config.node[:owner_name]}@#{config.node[:master_app_server][:private_dns_name]}:#{config_path} #{config_path}"
