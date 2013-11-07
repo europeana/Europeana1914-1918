a314e9ed28ef991474b8068f106afeca9fd4cb67
Richard Doe <richard.doe@rwdit.net>
2013-11-07 13:58:34 +0000

    Re #17: Add edm:provider and edm:dataProvider to story and item aggregations .

--- app/models/europeana/edm/mapping/story.rb
+++ app/models/europeana/edm/mapping/story.rb
@@ -259,6 +259,8 @@ module Europeana
             graph << [ uri, RDF::EDM.rights, RDF::URI.parse(meta["license"].first) ] 
           end
           graph << [ uri, RDF::EDM.ugc, "TRUE" ]
+          graph << [ uri, RDF::EDM.provider, "Europeana 1914-1918" ]
+          graph << [ uri, RDF::EDM.dataProvider, "Europeana 1914-1918" ]
           
           graph
         end
