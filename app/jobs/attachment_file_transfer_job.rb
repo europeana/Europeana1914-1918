334cf34f6e5505306278168f77bab603b9c25a2e
Richard Doe <richard.doe@rwdit.net>
2013-11-14 14:46:24 +0000

    Store tempfile name, not path.

--- app/jobs/attachment_file_transfer_job.rb
+++ app/jobs/attachment_file_transfer_job.rb
@@ -2,13 +2,13 @@ class AttachmentFileTransferJob
   def initialize(attachment_id, file_upload)
     @attachment_id = attachment_id
     
-    tempfile_path = File.join(Rails.root, 'tmp', 'files', File.basename(file_upload.tempfile.path) + File.extname(file_upload.original_filename))
-    FileUtils.mv(file_upload.tempfile.path, tempfile_path)
+    tempfile_name = File.basename(file_upload.tempfile.path) + File.extname(file_upload.original_filename)
+    FileUtils.mv(file_upload.tempfile.path, tempfile_path(tempfile_name))
     
     file_hash = {
       :content_type => file_upload.content_type.respond_to?(:content_type) ? file_upload.content_type.content_type : file_upload.content_type,
       :original_filename => file_upload.original_filename,
-      :tempfile_path => tempfile_path
+      :tempfile_name => tempfile_name
     }
     
     @file = file_hash
@@ -16,10 +16,17 @@ class AttachmentFileTransferJob
   
   def perform
     attachment = Attachment.find(@attachment_id)
-    attachment.file = File.open(@file[:tempfile_path])
+    attachment.file = File.open(tempfile_path(@file[:tempfile_name]))
     attachment.file_content_type = @file[:content_type]
     attachment.file_file_name = @file[:original_filename]
     attachment.save
-    File.unlink(@file[:tempfile_path])
+    File.unlink(tempfile_path(@file[:tempfile_name]))
   end
+  
+protected
+
+  def tempfile_path(tempfile_name)
+    File.join(Rails.root, 'tmp', 'files', tempfile_name)
+  end
+
 end
