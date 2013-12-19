78116c279fa1310f510cb05f9add45929325d0bf
Richard Doe <richard.doe@rwdit.net>
2013-12-19 10:27:16 +0000

    Re #166: Copy sample Solr config file, instead of symlinking.

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -8,4 +8,4 @@ run "ln -nfs #{config.shared_path}/config/initializers/action_mailer.rb #{config
 run "ln -nfs #{config.shared_path}/config/initializers/europeana.rb #{config.release_path}/config/initializers/europeana.rb"
 
 # Solr
-run "ln -nfs #{config.release_path}/solr/conf/solrconfig.36.xml #{config.release_path}/solr/conf/solrconfig.xml"
+run "cp #{config.release_path}/solr/conf/solrconfig.36.xml #{config.release_path}/solr/conf/solrconfig.xml"
