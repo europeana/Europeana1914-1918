1da99cfb4dab08e83c2d6c7e388a1ce35354824e
Richard Doe <richard.doe@rwdit.net>
2013-12-19 10:24:17 +0000

    Re #166: Separate sample Solr config files for Lucene 3.6 and 4.1.

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -6,3 +6,6 @@ run "ln -nfs #{config.shared_path}/config/initializers/action_mailer.rb #{config
 
 # Europeana API
 run "ln -nfs #{config.shared_path}/config/initializers/europeana.rb #{config.release_path}/config/initializers/europeana.rb"
+
+# Solr
+run "ln -nfs #{config.release_path}/solr/conf/solrconfig.36.xml #{config.release_path}/solr/conf/solrconfig.xml"
