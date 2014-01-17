abdc210072df16f683f6179352edd4d3ef40c92b
Andy MacLean <andyjmacLean@gmail.com>
2014-01-17 18:13:44 +0100

    layout

--- public/themes/v2.1/javascripts/eu/europeana/contributions-show.js
+++ public/themes/v2.1/javascripts/eu/europeana/contributions-show.js
@@ -564,160 +564,6 @@
 		}
 	},
 
-/*
-	map = {
-
-		init:function(){
-			if(typeof latLong != 'undefined' && latLong.length == 2){
-				var mapLatitude  = parseFloat(latLong[0]);
-				var mapLongitude = parseFloat(latLong[1]);
-				var mapZoom      = typeof mapZoom != 'undefined' && mapZoom.length && parseInt(mapZoom).length ? parseInt(mapZoom) : 8;
-
-				var rootJsUrl    = themePath + 'javascripts/com/leaflet/';
-				var rootCssUrl   = themePath + 'stylesheets/com/leaflet/';
-				var dependencies = [
-				    'http://maps.googleapis.com/maps/api/js?sensor=false&amp;libraries=places&amp;key=AIzaSyARYUuCXOrUv11afTLg72TqBN2n-o4XmCI',
-    				'leaflet.js',
-   					'Leaflet-MiniMap-master/src/Control.MiniMap.js',
-   					'Leaflet-Pan/L.Control.Pan.js',
-   					'leaflet-plugins-master/layer/tile/Google.js',
-   					'leaflet.min.css',
-   					'leaflet.ie.css',
-   					'Leaflet-MiniMap-master/src/Control.MiniMap.css'
-   				];
-
-            	var recursiveLoad = function(index){
-            		index = index ? index : 0;
-            		if(dependencies.length > index){
-    					if( dependencies[index].split('.').pop() == 'css'){
-    						$('head').append('<link rel="stylesheet" href="' + rootCssUrl + dependencies[index] + '" type="text/css"/>');
-        					recursiveLoad(index + 1);
-    					}
-    					else{
-    	          			$.ajax({
-                				"url": dependencies[index].indexOf('http') == 0 ? dependencies[index] :  rootJsUrl + dependencies[index],
-                				"dataType": "script",
-                				"success": function(){
-                					recursiveLoad(index + 1);
-                				},
-                	            "contentType" :	"application/x-www-form-urlencoded;charset=UTF-8"
-                			});
-    					}
-            		}
-            		else{
-            			var mqTilesAttr = 'Tiles &copy; <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" />';
-
-            			// map quest
-            			var mq = new L.TileLayer(
-            				'http://otile{s}.mqcdn.com/tiles/1.0.0/{type}/{z}/{x}/{y}.png',
-            				{
-            					minZoom: 4,
-            					maxZoom: 18,
-            					attribution: mqTilesAttr,
-            					subdomains: '1234',
-            					type: 'osm'
-            				}
-            			);
-
-            			var map = L.map('map', {
-            			    center: new L.LatLng(mapLatitude, mapLongitude),
-            			    zoomControl: false,
-            			    zoom: mapZoom
-            			});
-
-            			L.Icon.Default.imagePath = rootCssUrl + 'images/';
-        				L.marker([mapLatitude, mapLongitude]).addTo(map);
-
-            			var europeanaCtrls = jQuery('<div id="europeana-ctrls">').prependTo('#map-container');
-
-            			var EuropeanaLayerControl = function(map, ops){
-
-            				var self = this;
-
-            				self.ops = ops;
-            				self.map = map;
-            				self.grp = null;
-
-            				self._setLayer = function(index){
-            					var layer = self.ops[index].layer;
-            					self.grp.clearLayers();
-            					self.grp.addLayer(layer);
-
-            					jQuery(self.cmp.find("span")).removeClass('active');
-            					jQuery(self.cmp.find("span").get(index)).addClass('active');
-            				};
-
-            				var html	= '';
-            				var layers	= [];
-
-            				jQuery.each(self.ops, function(i, ob){
-            					html += '<a href="#' + ob.title + '"><span class="' + i + '">' + ob.title + '</span></a>';
-            					layers[layers.length] = ob.layer;
-            				});
-
-
-            				self.cmp = jQuery('<div id="layer-ctrl">' + html + '</div>');
-
-            				self.cmp.find("span").each(function(){
-            					jQuery(this).click(function(){
-            						self._setLayer(parseInt(jQuery(this).attr('class')));
-            					});
-            				});
-
-            				self.grp = L.layerGroup(layers);
-            				self.grp.addTo(self.map);
-            				self._setLayer(0);
-
-            				return {
-            					getCmp : function(index){
-            						return self.cmp;
-            					}
-            				}
-            			};
-
-            			var ggl = new L.Google();
-            			map.addLayer(ggl);
-
-            			var ctrlLayer = new EuropeanaLayerControl(map,
-            				[
-            					{
-            					    "title":	"Map",
-            					    "layer":	mq
-            				    },
-            				    {
-            					    "title":	"Satellite",
-            					    "layer":	ggl
-            				    }
-            			    ]
-            			);
-
-            			europeanaCtrls.append(ctrlLayer.getCmp());
-
-            			// Overview map - requires duplicate layer
-            			//var osm2 = new L.TileLayer(osmUrl, {minZoom: 0, maxZoom: 13, attribution: osmAttrib });
-            			new L.Control.MiniMap(
-            				new L.TileLayer(
-            					'http://otile{s}.mqcdn.com/tiles/1.0.0/{type}/{z}/{x}/{y}.png',
-            					{
-            						minZoom: 0,
-            						maxZoom: 13,
-            						attribution: mqTilesAttr,
-            						subdomains: '1234',
-            						type: 'osm'
-            					}),
-            				{toggleDisplay: true }
-            			).addTo(map);
-            			L.control.zoom().addTo(map);
-
-            		}
-            	}
-            	recursiveLoad();
-			}
-
-		}
-	},
-
-*/
 	pdf = {
 		handleClick : function( evt ) {
 			var $elm = jQuery(this),
@@ -756,22 +602,11 @@
 
 	(function() {
 		truncate.init();
-		
-		/* For moblie / desktop layouts we need the translate services to be in two different places.
-		 * Unfortunately since the js for these services is not object oriented (no use of the "new" operator)
-		 * and since the container name is hard-coded into the js the strategy is now to move the existing 
-		 * instance of the translate services on window resize.
-		 * */
 		RunCoCo.translation_services.init( $('.translate-area') );
-
 		carousels.init();
 		map.init();
 		lightbox.init();
 		pdf.init();
-
-		// fire fake resize event to trigger euRsz (in case window was loaded within mobile breakpoints)
-		setTimeout(function(){ $(window).trigger('resize'); }, 100);
-
 	}());
 
 }());
