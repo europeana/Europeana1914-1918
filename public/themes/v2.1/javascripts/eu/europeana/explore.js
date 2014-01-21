e37b2bcddfad5c0ffcd54fdfa002f77c6ea1a51e
dan-nl <d_entous@yahoo.com>
2014-01-20 23:01:12 -0800

    re: #205 - Browse page changes
    * initial work to set-up the page

--- public/themes/v2.1/javascripts/eu/europeana/explore.js
+++ public/themes/v2.1/javascripts/eu/europeana/explore.js
@@ -1,34 +1,11 @@
 /*global jQuery */
 /*jslint browser: true, regexp: true, white: true */
 (function() {
-
 	'use strict';
-
-
-	jQuery('#explore-featured').imagesLoaded(function() {
-		this.rCarousel();
-	});
-
-	jQuery('#explore-collection-days')
-		.rCarousel({
-			listen_to_arrow_keys : false,
-			item_width_is_container_width : false,
-			nav_button_size : 'small'
-		});
 
 	jQuery('#explore-editors-picks')
 		.readMore({
 			read_more_link : '#read-more'
-		});
-
-	jQuery('#explore-featured-categories ol')
-		.imagesLoaded(function() {
-			jQuery(this).masonry({
-				itemSelector : 'li',
-				columnWidth : 1,
-				isFitWidth : true,
-				isAnimated : true
-			});
 		});
 
 	jQuery('#q').autocomplete({
