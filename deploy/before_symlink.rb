5784c476d8f52f5bff26f9d8f503b1eb0c0c72e4
Richard Doe <richard.doe@rwdit.net>
2014-01-21 13:50:38 +0000

    Re #201: Update EY deploy hook for new Solr sample file paths.

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -8,4 +8,6 @@ run "ln -nfs #{config.shared_path}/config/initializers/action_mailer.rb #{config
 run "ln -nfs #{config.shared_path}/config/initializers/europeana.rb #{config.release_path}/config/initializers/europeana.rb"
 
 # Solr
+run "mkdir #{config.release_path}/solr"
+run "cp -r #{config.release_path}/config/solr/conf #{config.release_path}/solr"
 run "cp #{config.release_path}/solr/conf/solrconfig.36.xml #{config.release_path}/solr/conf/solrconfig.xml"
