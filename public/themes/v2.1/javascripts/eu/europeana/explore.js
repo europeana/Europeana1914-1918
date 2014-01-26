afb6e480ee2a854ac422b7a7a7c1577259692c15
Richard Doe <richard.doe@rwdit.net>
2014-01-26 09:28:15 +0000

    Re #234: Disable search auto-complete in theme v2.1

--- public/themes/v2.1/javascripts/eu/europeana/explore.js
+++ public/themes/v2.1/javascripts/eu/europeana/explore.js
@@ -8,11 +8,11 @@
 			read_more_link : '#read-more'
 		});
 
-	jQuery('#q').autocomplete({
-		minLength : 3,
-		source : document.location.protocol + '//' + document.location.host + '/suggest.json',
-		select: function() { var self = this; setTimeout( function() { jQuery(self).closest('form').submit(); }, 100 ); }
-	});
+//	jQuery('#q').autocomplete({
+//		minLength : 3,
+//		source : document.location.protocol + '//' + document.location.host + '/suggest.json',
+//		select: function() { var self = this; setTimeout( function() { jQuery(self).closest('form').submit(); }, 100 ); }
+//	});
 
 
 }());
