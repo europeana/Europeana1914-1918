72a21c48ce0ecdecdbef5230783e58e5b61c5b75
Richard Doe <richard.doe@rwdit.net>
2014-02-18 12:06:46 +0000

    Re #312: Admin routing fixes.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,9 @@ class AdminController < ApplicationController
   before_filter :authenticate_user!
   before_filter :authorize!
 
+  def index
+  end
+
   protected
   def authorize!
     current_user.may_access_admin_area!
