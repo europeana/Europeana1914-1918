20303fb46f11e43fc4de0d21689a78974592c519
Richard Doe <richard.doe@rwdit.net>
2014-02-19 14:09:41 +0000

    Use SSMTP config file from app shared config directory.

--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -12,16 +12,9 @@ if [ "app", "util" ].include?(config.current_role)
     "#{config.shared_path}/config/google_api_key.p12",
     "#{config.shared_path}/config/sass.yml",
     "#{config.shared_path}/config/s3.yml",
+    "#{config.shared_path}/config/ssmtp/ssmtp.conf"
   ].each do |config_path|
     run "mkdir -p " + File.dirname(config_path)
     run "scp -o StrictHostKeyChecking=no -i /home/#{username}/.ssh/internal #{username}@#{config.node[:master_app_server][:private_dns_name]}:#{config_path} #{config_path}"
   end
 end
-
-# Copy SSMTP config file from app master to app and utility instances
-if [ "app", "util" ].include?(config.current_role)
-  config_path = "/data/ssmtp/ssmtp.conf"
-  sudo "mkdir -p " + File.dirname(config_path)
-  sudo "scp -o StrictHostKeyChecking=no -i /home/#{username}/.ssh/internal #{username}@#{config.node[:master_app_server][:private_dns_name]}:#{config_path} #{config_path}"
-end
-
--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -11,3 +11,6 @@ run "ln -nfs #{config.shared_path}/config/initializers/europeana.rb #{config.rel
 run "mkdir #{config.release_path}/solr"
 run "cp -r #{config.release_path}/config/solr/conf #{config.release_path}/solr"
 run "cp #{config.release_path}/solr/conf/solrconfig.36.xml #{config.release_path}/solr/conf/solrconfig.xml"
+
+# SSMTP
+sudo "ln -nfs #{config.shared_path}/config/ssmtp/ssmtp.conf /etc/ssmtp/ssmtp.conf"
