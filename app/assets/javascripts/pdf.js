820d7b61dd079e924235de677e38fe6f3dbbddec
Richard Doe <richard.doe@rwdit.net>
2014-03-05 14:11:52 +0000

    Re 301: PDF reader asset path adjustment.

---
+++ app/assets/javascripts/pdf.js
@@ -0,0 +1,5 @@
+//= require ./mozilla/pdf.js/compatibility
+//= require ./mozilla/pdf.js/l10n
+//= require ./mozilla/pdf.js/pdf
+//= require ./mozilla/pdf.js/debugger
+//= require ./mozilla/pdf.js/viewer
