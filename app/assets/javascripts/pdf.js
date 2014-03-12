b15ea6d1aa9adea1da709c3c9021b23a436de1dd
dan-nl <d_entous@yahoo.com>
2014-03-12 13:36:13 +0100

    * attempting to fix window undefined error by re-arranging the asset combining order

--- app/assets/javascripts/pdf.js
+++ app/assets/javascripts/pdf.js
@@ -1,5 +1,5 @@
+//= require ./mozilla/pdf.js/pdf
 //= require ./mozilla/pdf.js/viewer
 //= require ./mozilla/pdf.js/compatibility
 //= require ./mozilla/pdf.js/l10n
-//= require ./mozilla/pdf.js/pdf
 //= require ./mozilla/pdf.js/debugger
