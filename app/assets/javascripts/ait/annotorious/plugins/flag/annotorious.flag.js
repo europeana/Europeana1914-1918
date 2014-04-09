a4cc9efb958069607c4e39d110b76a46d6ca8f77
dan-nl <d_entous@yahoo.com>
2014-04-09 10:33:18 +0200

    Re #340 - Front end for flagging image annotations as inappropriate
    * js linted annotorious.runcoco.js
    * 2nd draft of annotorious.flag

--- app/assets/javascripts/ait/annotorious/plugins/flag/annotorious.flag.js
+++ app/assets/javascripts/ait/annotorious/plugins/flag/annotorious.flag.js
@@ -1,19 +1,76 @@
-annotorious.plugin.Flag = function() {
-	return true;
-};
+/*global annotorious, jQuery */
+/*jslint browser: true, nomen: true, regexp: true, white: true */
+(function ( annotorious, $ ) {
+	'use strict';
 
-annotorious.plugin.Flag.prototype.onInitAnnotator = function( annotator ) {
+	var Flag = {};
 
-	annotator.popup.addField( function( annotation ) {
+	annotorious.plugin.Flag = function( config ) {
+		this._BASE_URL = config.base_url;
+		Flag = this;
+	};
+
+	annotorious.plugin.Flag.prototype.onInitAnnotator = function( annotator ) {
+		annotator.popup.addField( Flag.addField );
+	};
+
+	annotorious.plugin.Flag.prototype.addField = function( annotation ) {
+		var $field = '';
+
 		if ( annotation.flaggable ) {
-			return jQuery('<a>')
-				.attr('class','annotorious-popup-button annotorious-popup-button-flag')
-				.attr('title','Flag')
-				.attr('href','javascript:void(0);')
-				.text('FLAG')
-				.on('click', function() { console.log( annotation ); })[0];
+			$field = $('<a>')
+				.attr( 'class','annotorious-popup-button annotorious-popup-flag-gray' )
+				.attr( 'title','Flag' )
+				.attr( 'href','#' )
+				.text( 'FLAG' )
+				.on( 'click', { annotation: annotation }, Flag.handleFlagClick )[0];
 		}
 
-		return false;
-  });
-};
+		return $field;
+	};
+
+	annotorious.plugin.Flag.prototype.handleFlagClick = function( evt ) {
+		var $elm = $(this),
+		annotation = evt.data.annotation;
+
+		evt.preventDefault();
+
+		if ( annotation.flaggable ) {
+			Flag.flagAnnotation( annotation, $elm );
+		}
+	};
+
+  annotorious.plugin.Flag.prototype._preserveCSRFToken = function(xhr) {
+    xhr.setRequestHeader(
+      "X-CSRF-Token",
+      $('meta[name="csrf-token"]').attr('content')
+    );
+  };
+
+  annotorious.plugin.Flag.prototype.flagAnnotation = function( annotation, $elm ) {
+    if ( !annotation.id ) {
+      return;
+    }
+
+    $.ajax({
+      type: "POST",
+      url: Flag._BASE_URL + "/" + annotation.id + "/flag",
+      beforeSend: Flag._preserveCSRFToken,
+			data: { '_method': 'put' },
+			success: Flag.toggleFlagIcon( $elm )
+    });
+  };
+
+	annotorious.plugin.Flag.prototype.toggleFlagIcon = function( $elm ) {
+		if ( $elm.hasClass( 'annotorious-popup-flag-gray' ) ) {
+			$elm
+				.removeClass( 'annotorious-popup-flag-gray' )
+				.addClass( 'annotorious-popup-flag-red' );
+		} else {
+			$elm
+				.removeClass( 'annotorious-popup-flag-red' )
+				.addClass( 'annotorious-popup-flag-gray' );
+		}
+	};
+
+}( annotorious, jQuery ));
--- app/assets/javascripts/ait/annotorious/plugins/runcoco/annotorious.runcoco.js
+++ app/assets/javascripts/ait/annotorious/plugins/runcoco/annotorious.runcoco.js
@@ -1,84 +1,108 @@
+/*global annotorious, anno, jQuery */
+/*jslint browser: true, nomen: true, regexp: true, white: true */
 /**
  * Annotorious storage plugin for RunCoCo
  */
-annotorious.plugin.RunCoCo = function(config) {
-  // Base URL of the RESTful annotations controller
-  this._BASE_URL = config.base_url;
-};
+(function( annotorious, anno, $ ) {
+  'use strict';
 
-annotorious.plugin.RunCoCo.prototype.initPlugin = function(anno) {
-  var self = this;
-  
-  anno.addHandler('onAnnotationCreated', function(annotation) {
-    self._create(annotation);
-  });
+  annotorious.plugin.RunCoCo = function(config) {
+    // Base URL of the RESTful annotations controller
+    this._BASE_URL = config.base_url;
+  };
 
-  anno.addHandler('onAnnotationUpdated', function(annotation) {
-    self._update(annotation);
-  });
+  annotorious.plugin.RunCoCo.prototype.initPlugin = function(anno) {
+    var self = this;
 
-  anno.addHandler('onAnnotationRemoved', function(annotation) {
-    self._delete(annotation);
-  });
-};
+    anno.addHandler('onAnnotationCreated', function(annotation) {
+      self._create(annotation);
+    });
 
-annotorious.plugin.RunCoCo.prototype.onInitAnnotator = function(annotator) {
-  this._loadAnnotations(annotator);
-};
+    anno.addHandler('onAnnotationUpdated', function(annotation) {
+      self._update(annotation);
+    });
 
-annotorious.plugin.RunCoCo.prototype._preserveCSRFToken = function(xhr) {
-  xhr.setRequestHeader("X-CSRF-Token", jQuery('meta[name="csrf-token"]').attr('content'));
-};
+    anno.addHandler('onAnnotationRemoved', function(annotation) {
+      self._delete(annotation);
+    });
+  };
 
-annotorious.plugin.RunCoCo.prototype._loadAnnotations = function(annotator) {
-  var self = this;
-  var params = { src: annotator.getItem().src };
-  
-  jQuery.ajax({
-    type: "GET",
-    url: self._BASE_URL + ".json", 
-    data: params,
-    beforeSend: self._preserveCSRFToken,
-    success: function(response) {
-      response.creatable ? anno.showSelectionWidget(params.src) : anno.hideSelectionWidget(params.src);
-      jQuery.each(response.annotations, function(idx, annotation) {
-        annotation.src = params.src;
-        anno.addAnnotation(annotation);
-      });
-    }
-  });
-};
+  annotorious.plugin.RunCoCo.prototype.onInitAnnotator = function(annotator) {
+    this._loadAnnotations(annotator);
+  };
 
-annotorious.plugin.RunCoCo.prototype._create = function(annotation) {
-  var self = this;
-  var params = { annotation: annotation };
-  jQuery.ajax({
-    type: "POST",
-    url: self._BASE_URL + ".json", 
-    data: params,
-    beforeSend: self._preserveCSRFToken,
-    success: function(response) {
-      annotation.id = response.id;
-    }
-  });
-};
+  annotorious.plugin.RunCoCo.prototype._preserveCSRFToken = function(xhr) {
+    xhr.setRequestHeader(
+      "X-CSRF-Token",
+      $('meta[name="csrf-token"]').attr('content')
+    );
+  };
 
-annotorious.plugin.RunCoCo.prototype._update = function(annotation) {
-  var self = this;
-  var params = { annotation: annotation };
-  jQuery.ajax({
-    type: "PUT",
-    url: self._BASE_URL + "/" + annotation.id + ".json", 
-    data: params,
-    beforeSend: self._preserveCSRFToken
-  });
-};
+  annotorious.plugin.RunCoCo.prototype._loadAnnotations = function(annotator) {
+    var self = this,
+    params = {
+      src: annotator.getItem().src
+    };
 
-annotorious.plugin.RunCoCo.prototype._delete = function(annotation) {
-  var self = this;
-  jQuery.ajax({
-    type: "DELETE",
-    url: self._BASE_URL + "/" + annotation.id + ".json", 
-    beforeSend: self._preserveCSRFToken
-  });
-};
+    $.ajax({
+      type: "GET",
+      url: self._BASE_URL + ".json",
+      data: params,
+      beforeSend: self._preserveCSRFToken,
+      success: function(response) {
+        response.creatable =
+          response.creatable
+          ? anno.showSelectionWidget(params.src)
+          : anno.hideSelectionWidget(params.src);
+
+        $.each(response.annotations, function() {
+          var annotation = this;
+          annotation.src = params.src;
+          anno.addAnnotation(annotation);
+        });
+      }
+    });
+  };
+
+  annotorious.plugin.RunCoCo.prototype._create = function(annotation) {
+    var self = this,
+    params = {
+      annotation: annotation
+    };
+
+    $.ajax({
+      type: "POST",
+      url: self._BASE_URL + ".json",
+      data: params,
+      beforeSend: self._preserveCSRFToken,
+      success: function(response) {
+        annotation.id = response.id;
+      }
+    });
+  };
+
+  annotorious.plugin.RunCoCo.prototype._update = function(annotation) {
+    var self = this,
+    params = {
+      annotation: annotation
+    };
+
+    $.ajax({
+      type: "PUT",
+      url: self._BASE_URL + "/" + annotation.id + ".json",
+      data: params,
+      beforeSend: self._preserveCSRFToken
+    });
+  };
+
+  annotorious.plugin.RunCoCo.prototype._delete = function(annotation) {
+    var self = this;
+
+    $.ajax({
+      type: "DELETE",
+      url: self._BASE_URL + "/" + annotation.id + ".json",
+      beforeSend: self._preserveCSRFToken
+    });
+  };
+
+}( annotorious, anno, jQuery ));
