d37a1943d85860018eb21045ecbbc1fe802bee88
dan-nl <d_entous@yahoo.com>
2014-04-10 09:42:29 +0200

    Re #340 - Front end for flagging image annotations as inappropriate
    * added code comments
    * added unflag call in anticipation of the new all method
    * added appropriate toggling of the flag icon click listener depending on the state of the flag

--- app/assets/javascripts/ait/annotorious/plugins/flag/annotorious.flag.js
+++ app/assets/javascripts/ait/annotorious/plugins/flag/annotorious.flag.js
@@ -1,15 +1,24 @@
 /*global annotorious, I18n, jQuery */
 /*jslint browser: true, nomen: true, regexp: true, white: true */
+/**
+ * flag plugin for annotorious developed for europeana 1914-1918
+ */
 (function ( annotorious, I18n, $ ) {
 	'use strict';
 
 	var Flag = {};
 
+	/**
+	 * @param {object} config
+	 */
 	annotorious.plugin.Flag = function( config ) {
 		this._BASE_URL = config.base_url;
 		Flag = this;
 	};
 
+	/**
+	 * @param {object} annotator
+	 */
 	annotorious.plugin.Flag.prototype.onInitAnnotator = function( annotator ) {
 		Flag.preloadImages();
 		annotator.popup.addField( Flag.addFlagIcon );
@@ -24,6 +33,10 @@
 		image[1].src = '/assets/ait/annotorious/plugins/flag/flag-red.png';
 	};
 
+	/**
+	 * @param {object} annotation
+	 * @returns {object} jQuery object representing the flag icon
+	 */
 	annotorious.plugin.Flag.prototype.addFlagIcon = function( annotation ) {
 		if ( !annotation.flaggable ) {
 			return;
@@ -49,6 +62,9 @@
 		return $flag_icon[0];
 	};
 
+	/**
+	 * @param {evt} jQuery Event object
+	 */
 	annotorious.plugin.Flag.prototype.handleFlagClick = function( evt ) {
 		var $elm = $(this),
 		annotation = evt.data.annotation;
@@ -58,38 +74,67 @@
 
 		if ( !annotation.flagged ) {
 			Flag.flagAnnotation( annotation, $elm );
+		} else {
+			Flag.unflagAnnotation( annotation, $elm );
 		}
 	};
 
-  annotorious.plugin.Flag.prototype._preserveCSRFToken = function(xhr) {
-    xhr.setRequestHeader(
+	/**
+	 * @param {object} annotation
+	 * @param {object} $elm jQuery object representing the flag icon
+	 */
+	annotorious.plugin.Flag.prototype.toggleFlagIcon = function( annotation, $elm ) {
+		if ( $elm.hasClass( 'annotorious-popup-flag-gray' ) ) {
+			$elm
+				.removeClass( 'annotorious-popup-flag-gray' )
+				.addClass( 'annotorious-popup-flag-red' )
+				.text( I18n.t('javascripts.annotorious.clear-flag') )
+				.on( 'click', { annotation: annotation }, Flag.handleFlagClick );
+		} else {
+			$elm
+				.removeClass( 'annotorious-popup-flag-red' )
+				.addClass( 'annotorious-popup-flag-gray' )
+				.text( I18n.t('javascripts.annotorious.flag') )
+				.on( 'click', { annotation: annotation }, Flag.handleFlagClick );
+		}
+	};
+
+	/**
+	 * @param {object} jqXHR jQuery XHR object
+	 */
+  annotorious.plugin.Flag.prototype._preserveCSRFToken = function( jqXHR ) {
+    jqXHR.setRequestHeader(
       "X-CSRF-Token",
       $('meta[name="csrf-token"]').attr('content')
     );
   };
 
+	/**
+	 * @param {object} annotation
+	 * @param {object} $elm jQuery object representing the flag icon
+	 */
   annotorious.plugin.Flag.prototype.flagAnnotation = function( annotation, $elm ) {
     $.ajax({
       type: "POST",
       url: Flag._BASE_URL + "/" + annotation.id + "/flag",
       beforeSend: Flag._preserveCSRFToken,
 			data: { '_method': 'put' },
-			success: function() { annotation.flagged = true; Flag.toggleFlagIcon( $elm ); }
+			success: function() { annotation.flagged = true; Flag.toggleFlagIcon( annotation, $elm ); }
     });
   };
 
-	annotorious.plugin.Flag.prototype.toggleFlagIcon = function( $elm ) {
-		if ( $elm.hasClass( 'annotorious-popup-flag-gray' ) ) {
-			$elm
-				.removeClass( 'annotorious-popup-flag-gray' )
-				.addClass( 'annotorious-popup-flag-red' )
-				.text( I18n.t('javascripts.annotorious.clear-flag') );
-		} else {
-			$elm
-				.removeClass( 'annotorious-popup-flag-red' )
-				.addClass( 'annotorious-popup-flag-gray' )
-				.text( I18n.t('javascripts.annotorious.flag') );
-		}
-	};
+	/**
+	 * @param {object} annotation
+	 * @param {object} $elm jQuery object representing the flag icon
+	 */
+	annotorious.plugin.Flag.prototype.unflagAnnotation = function( annotation, $elm ) {
+    $.ajax({
+      type: "POST",
+      url: Flag._BASE_URL + "/" + annotation.id + "/unflag",
+      beforeSend: Flag._preserveCSRFToken,
+			data: { '_method': 'put' },
+			success: function() { annotation.flagged = true; Flag.toggleFlagIcon( annotation, $elm ); }
+    });
+  };
 
 }( annotorious, I18n, jQuery ));
