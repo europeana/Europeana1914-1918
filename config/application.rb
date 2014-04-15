7e8648ea530c4c9bcc35fc9ab1b842f9c0df0934
Richard Doe <richard.doe@rwdit.net>
2014-04-15 16:58:59 +0100

    Disable persistent sweepers (prevented db:setup from succeeding).

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,8 @@ module RunCoCo
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
     # Activate observers that should always be running.
-    config.active_record.observers = "AttachmentSweeper", "ContactSweeper",
-      "ContributionSweeper"
+    # config.active_record.observers = "AttachmentSweeper", "ContactSweeper",
+    #   "ContributionSweeper"
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
