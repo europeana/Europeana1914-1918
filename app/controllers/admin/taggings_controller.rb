a3f6bca3f8f2676d6858148809f056e60eaf8fad
Richard Doe <richard.doe@rwdit.net>
2014-04-15 12:43:37 +0100

    Re #319: Admin UI for contribution taggings.

---
+++ app/controllers/admin/taggings_controller.rb
@@ -0,0 +1,15 @@
+class Admin::TaggingsController < AdminController
+  # GET /admin/taggings
+  def index
+    count = [ (params[:count] || 20).to_i, 100 ].min # Default 20, max 100
+    page = (params[:page] || 1).to_i
+
+    params[:col] = params[:col].to_s.downcase
+    params[:col] = 'created_at' unless [ 'created_at', 'text', 'tags.name' ].include?(params[:col])
+
+    params[:order] = params[:order].to_s.upcase
+    params[:order] = 'DESC' unless [ 'ASC', 'DESC' ].include?(params[:order])
+    
+    @taggings = ActsAsTaggableOn::Tagging.where(:context => 'tags').includes('tag').join_current_status.order("#{params[:col]} #{params[:order]}").paginate(:page => params[:page])
+  end
+end
