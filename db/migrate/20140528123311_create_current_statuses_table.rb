04206c8b0e0a90145b1fe50ad05b481a3563b6ff
Richard Doe <richard.doe@rwdit.net>
2014-05-28 14:31:58 +0100

    Ensure only one CurrentStatus saved per record in migration.

--- db/migrate/20140528123311_create_current_statuses_table.rb
+++ db/migrate/20140528123311_create_current_statuses_table.rb
@@ -10,8 +10,14 @@ class CreateCurrentStatusesTable < ActiveRecord::Migration
     add_index "current_statuses", "record_type"
     
     RecordStatus.find_each do |rs|
-      unless CurrentStatus.where("record_id = ? AND record_type = ? AND updated_at > ?", rs.record_id, rs.record_type, rs.created_at).count > 0
-        cs = CurrentStatus.new(:record_id => rs.record_id, :record_type => rs.record_type, :name => rs.name, :created_at => rs.created_at, :updated_at => rs.created_at)
+      unless CurrentStatus.where("record_id = ? AND record_type = ? AND created_at > ?", rs.record_id, rs.record_type, rs.created_at).count > 0
+        if cs = CurrentStatus.where("record_id = ? AND record_type = ?", rs.record_id, rs.record_type).first
+          cs.name = rs.name
+          cs.updated_at = rs.created_at
+          cs.created_at = rs.created_at
+        else
+          cs = CurrentStatus.new(:record_id => rs.record_id, :record_type => rs.record_type, :name => rs.name, :created_at => rs.created_at, :updated_at => rs.created_at)
+        end
         cs.save!
       end
     end
