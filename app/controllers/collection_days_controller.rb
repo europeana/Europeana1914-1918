ab7b36926d46577bb2e67195531cdaff447dbfc9
Richard Doe <richard.doe@rwdit.net>
2014-06-10 13:05:48 +0100

    Re #420: Order collection days by country ASC, city ASC, start date DESC, end date DESC.

--- app/controllers/collection_days_controller.rb
+++ app/controllers/collection_days_controller.rb
@@ -1,7 +1,13 @@
 class CollectionDaysController < ApplicationController
   def index
     @collection_days = CollectionDay.includes(:taxonomy_term, :contact).all
-    @collection_days.sort_by! { |cd| [ I18n.t("countries.#{cd.contact.country}"), cd.name ] }
+    @collection_days.sort! { |a, b| 
+      (I18n.t("countries.#{a.contact.country}") <=> I18n.t("countries.#{b.contact.country}")).nonzero? ||
+      (a.name <=> b.name).nonzero? ||
+      (b.start_date <=> a.start_date).nonzero? ||
+      (b.end_date <=> a.end_date).nonzero? ||
+      0
+    }
   end
   
   def show
