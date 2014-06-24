ddadb265e296b3c37693fe5e3dd2d4b949f310f7
Richard Doe <richard.doe@rwdit.net>
2014-06-24 09:43:13 +0100

    Set I18n.available_locales from presence of LOCALE.yml files in config/locales.

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,7 @@ module RunCoCo
     # The default locale is :en and all translations from config/locales/*/*.rb,yml are auto loaded.
     config.i18n.load_path = Dir[Rails.root.join('config', 'locales', '*', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
+    config.i18n.available_locales = Dir[Rails.root.join('config', 'locales', '*.yml').to_s].map { |yml| File.basename(yml, '.yml') }
 
     # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
     # the I18n.default_locale when a translation can not be found)
