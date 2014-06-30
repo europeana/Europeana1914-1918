0c24c47585982f7ae22735aed1e2262f77fbeed3
dan-nl <d_entous@yahoo.com>
2014-06-30 17:42:10 +0200

    Re: #450 - BUG: pdfs not displaying
    * updated to the latest version of pdf.js

--- app/assets/javascripts/pdf.js
+++ app/assets/javascripts/pdf.js
@@ -1,4 +1,4 @@
-//= require ./mozilla/pdf.js/pdf
-//= require ./mozilla/pdf.js/l10n
 //= require ./mozilla/pdf.js/compatibility
+//= require ./mozilla/pdf.js/l10n
+//= require ./mozilla/pdf.js/pdf
 //= require ./mozilla/pdf.js/viewer
