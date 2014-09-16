1d26c1f80366618a29ef4c84a5d38bc370c15259
Richard Doe <richard.doe@rwdit.net>
2014-09-16 10:54:16 +0100

    Re #454: Adjust RunCoCo logger for ActiveSupport 3.2 compatibility

--- lib/runcoco/logger.rb
+++ lib/runcoco/logger.rb
@@ -10,15 +10,8 @@ module RunCoCo
     end
 
     def add(severity, message = nil, progname = nil, &block)
-      return if @level > severity
-      message = (message || (block && block.call) || progname).to_s
-      # If a newline is necessary then create a new message ending with a newline.
-      # Ensures that the original message is not mutated.
-      message = "#{message}\n" unless message[-1] == ?\n
       message = [Time.zone.now.strftime("%Y-%m-%d %H:%M:%S"), SEVERITY_NAME[severity], message].join(' ')
-      buffer << message
-      auto_flush
-      message
+      super(severity, message, progname, &block)
     end
   end
 end
