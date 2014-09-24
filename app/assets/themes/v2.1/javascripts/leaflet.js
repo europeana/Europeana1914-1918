9563b5e32f36efe04beebf23bf8d038e61270c7c
dan-nl <d_entous@yahoo.com>
2014-09-24 12:59:59 +0200

    Re: #435 - Collection Days: leaflet map, mobile implementation
    * updated leaflet.js

--- app/assets/themes/v2.1/javascripts/leaflet.js
+++ app/assets/themes/v2.1/javascripts/leaflet.js
@@ -1,4 +1,4 @@
-//= require leaflet/leaflet-0.7.2-src
+//= require leaflet/leaflet-0.7.3-src
 //= require leaflet/plugins/Control.MiniMap
 //= require leaflet/plugins/Class.Google
 //= require leaflet/plugins/Control.LayerToggle
