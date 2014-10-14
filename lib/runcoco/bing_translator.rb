4fffbec089c723f146bcf5596468e39e9ec6a3d8
Richard Doe <richard.doe@rwdit.net>
2014-10-14 11:31:07 +0100

    Re #489: Cache Microsoft Translator supported language codes for one month.

--- lib/runcoco/bing_translator.rb
+++ lib/runcoco/bing_translator.rb
@@ -12,6 +12,10 @@ module RunCoCo
       def configured?
         RunCoCo.configuration.bing_client_id.present? && RunCoCo.configuration.bing_client_secret.present?
       end
+      
+      def translator
+        @translator ||= ::BingTranslator.new(RunCoCo.configuration.bing_client_id, RunCoCo.configuration.bing_client_secret)
+      end
 
       ##
       # Translates text from the current locale to app's other locales.
@@ -26,13 +30,11 @@ module RunCoCo
       def translate(text, from_locale = I18n.locale)
         return text unless text.present? && configured?
 
-        translator = ::BingTranslator.new(RunCoCo.configuration.bing_client_id, RunCoCo.configuration.bing_client_secret)
-
         Rails.logger.debug("Using Bing Translate API to translate \"#{text}\" from #{from_locale}...")
         translations = { from_locale => text }
 
         other_locales = I18n.available_locales.reject { |locale| locale == from_locale }
-        other_locales.select! { |locale| translator.supported_language_codes.include?(locale.to_s) }
+        other_locales.select! { |locale| supported_language_codes.include?(locale.to_s) }
         other_locales.each do |to_locale|
           Rails.logger.debug("... to #{to_locale}")
           translations[to_locale] = translator.translate(text, :from => from_locale, :to => to_locale)
@@ -53,6 +55,28 @@ module RunCoCo
         end
 
         return token
+      end
+      
+      def supported_language_codes
+        cache_key = "bing_translator/supported_language_codes"
+        
+        if Rails.cache.exist?(cache_key)
+          codes = YAML::load(Rails.cache.read(cache_key))
+        else
+          begin
+            codes = translator.supported_language_codes
+            expiration = Rails.configuration.cache_store.first == :dalli_store ? 1.month.from_now.to_i : 1.month
+            Rails.cache.write(cache_key, codes.to_yaml, :expires_in => expiration)
+          rescue Exception => e
+            if e.message.match(/TranslateApiException/)
+              codes = []
+            else
+              raise e
+            end
+          end
+        end
+        
+        codes
       end
     end
   end
