29ae1ca8647de4bd15df1e4330a81f2d206ae15c
andyjmaclean <andyjmaclean@gmail.com>
2016-10-14 11:15:55 +0200

    Re: #1393 test

--- app/assets/javascripts/europeana/lightbox.js
+++ app/assets/javascripts/europeana/lightbox.js
@@ -2,231 +2,233 @@
 /*jslint browser: true, white: true */
 (function( $ ) {
 
-	'use strict';
+  'use strict';
 
-	if ( !window.europeana ) {
-		window.europeana = {};
-	}
-
-
-	europeana.lightbox = {
-
-		annotorious_setup: false,
-		current : 0,
-		$metadata : [],
-		options: {
-			add_embedly: false,
-			add_lightbox: false,
-			add_metadata: false,
-			add_sharethis: false,
-			carousel: {},
-			contribution_page: false,
-			edm_page: false
-		},
-		ppOptions : {},
+  if ( !window.europeana ) {
+    window.europeana = {};
+  }
 
 
-		/**
-		 * @param {object} $elm
-		 * jQuery object
-		 */
-		addMetaDataOverlay : function( $elm ) {
-			var
-			self = this,
-			$pic_full_res = jQuery('#pp_full_res'),
-			$pp_content = jQuery('.pp_content');
+  europeana.lightbox = {
 
-			if ( !self.$metadata[self.current] ) {
-				self.$metadata[self.current] = ( jQuery( $elm.attr('href') ) );
-				self.$metadata[ self.current ].data('clone', self.$metadata[ self.current ].clone() );
-			}
+    annotorious_setup: false,
+    current : 0,
+    $metadata : [],
+    options: {
+      add_embedly: false,
+      add_lightbox: false,
+      add_metadata: false,
+      add_sharethis: false,
+      carousel: {},
+      contribution_page: false,
+      edm_page: false
+    },
+    ppOptions : {},
 
-			self.$metadata[ self.current ].data('clone').appendTo( $pp_content );
 
-			self.$metadata[ self.current ].data('clone').css({
-				height :
-					$pic_full_res.find('img').height() -
-					parseInt( self.$metadata[ self.current ].data('clone').css('padding-top'), 10 ) -
-					parseInt( self.$metadata[ self.current ].data('clone').css('padding-bottom'), 10 )
-			});
+    /**
+     * @param {object} $elm
+     * jQuery object
+     */
+    addMetaDataOverlay : function( $elm ) {
+      var
+      self = this,
+      $pic_full_res = jQuery('#pp_full_res'),
+      $pp_content = jQuery('.pp_content');
 
-			$pic_full_res.append( self.$metadata[ self.current ].find('.metadata-license').html() );
-		},
+      if ( !self.$metadata[self.current] ) {
+        self.$metadata[self.current] = ( jQuery( $elm.attr('href') ) );
+        self.$metadata[ self.current ].data('clone', self.$metadata[ self.current ].clone() );
+      }
 
-		handleMetaDataClick : function( evt ) {
-			var self = evt.data.self;
-			evt.preventDefault();
-			self.$metadata[self.current].data('clone').slideToggle();
-		},
+      self.$metadata[ self.current ].data('clone').appendTo( $pp_content );
 
-		handlePageChangeNext : function( keyboard ) {
-			if ( !keyboard ) {
-				europeana.lightbox.options.carousel.$featured_carousel.$nav_next.trigger('click');
-			}
-		},
+      self.$metadata[ self.current ].data('clone').css({
+        height :
+          $pic_full_res.find('img').height() -
+          parseInt( self.$metadata[ self.current ].data('clone').css('padding-top'), 10 ) -
+          parseInt( self.$metadata[ self.current ].data('clone').css('padding-bottom'), 10 )
+      });
 
-		handlePageChangePrev : function( keyboard ) {
-			if ( !keyboard ) {
-				europeana.lightbox.options.carousel.$featured_carousel.$nav_prev.trigger('click');
-			}
-		},
+      $pic_full_res.append( self.$metadata[ self.current ].find('.metadata-license').html() );
+    },
 
-		/**
-		 *	this - refers to the generated lightbox div
-		 *	the div is removed each time the lightbox is closed
-		 *	so these elements need to be added back to the div
-		 *	with each open
-		 */
-		handlePictureChange : function() {
-			var self = europeana.lightbox,
-			$elm = jQuery(this),
-			$lightbox_description = $elm.find('.pp_description'),
-			$additional_info_link = $lightbox_description.find('a.lightbox-info').first();
+    handleMetaDataClick : function( evt ) {
+      var self = evt.data.self;
+      evt.preventDefault();
+      self.$metadata[self.current].data('clone').slideToggle();
+    },
 
-			anno.reset();
-			anno.hideSelectionWidget();
+    handlePageChangeNext : function( keyboard ) {
+      if ( !keyboard ) {
+        europeana.lightbox.options.carousel.$featured_carousel.$nav_next.trigger('click');
+      }
+    },
 
-			if ( self.options.add_metadata ) {
-				if ( self.$metadata[self.current] ) {
-					if ( self.$metadata[self.current].data('clone').is(':visible') ) {
-						self.$metadata[self.current].data('clone').hide();
-					}
+    handlePageChangePrev : function( keyboard ) {
+      if ( !keyboard ) {
+        europeana.lightbox.options.carousel.$featured_carousel.$nav_prev.trigger('click');
+      }
+    },
 
-					if ( self.$metadata[self.current].data('cloned') ) {
-						self.$metadata[self.current].data('cloned', false);
-					}
-				}
+    /**
+     *	this - refers to the generated lightbox div
+     *	the div is removed each time the lightbox is closed
+     *	so these elements need to be added back to the div
+     *	with each open
+     */
+    handlePictureChange : function() {
+      var self = europeana.lightbox,
+      $elm = jQuery(this),
+      $lightbox_description = $elm.find('.pp_description'),
+      $additional_info_link = $lightbox_description.find('a.lightbox-info').first();
 
-				$additional_info_link.on('click', { self : self }, self.handleMetaDataClick );
-				self.current = parseInt( $additional_info_link.attr('href').replace('#inline-',''), 10 );
-				self.addMetaDataOverlay( $additional_info_link );
-			}
+      anno.reset();
+      anno.hideSelectionWidget();
 
-			if ( self.options.add_sharethis ) {
-				europeana.sharethis.manageShareThis( $('#inline-' + self.current ), $lightbox_description, self.current );
-			}
+      if ( self.options.add_metadata ) {
+        if ( self.$metadata[self.current] ) {
+          if ( self.$metadata[self.current].data('clone').is(':visible') ) {
+            self.$metadata[self.current].data('clone').hide();
+          }
 
-			if ( self.options.add_embedly ) {
-				europeana.embedly.manageEmbedly( $('#inline-' + self.current ), $lightbox_description, self.current );
-			}
-		},
+          if ( self.$metadata[self.current].data('cloned') ) {
+            self.$metadata[self.current].data('cloned', false);
+          }
+        }
 
-		hideLightboxContent: function() {
-			var $pp_pic_holder = $('.pp_pic_holder');
-			$pp_pic_holder.find('#pp_full_res object,#pp_full_res embed').css('visibility','hidden');
-			$pp_pic_holder.find('.pp_fade').fadeOut('fast',function(){
-				$('.pp_loaderIcon').show();
-			});
-		},
+        $additional_info_link.on('click', { self : self }, self.handleMetaDataClick );
+        self.current = parseInt( $additional_info_link.attr('href').replace('#inline-',''), 10 );
+        self.addMetaDataOverlay( $additional_info_link );
+      }
 
-		/**
-		 * @param {bool} options
-		 */
-		init : function( options ) {
-			this.options = $.extend( true, {}, this.options, options );
+      if ( self.options.add_sharethis ) {
+        europeana.sharethis.manageShareThis( $('#inline-' + self.current ), $lightbox_description, self.current );
+      }
 
-			if ( this.options.add_lightbox ) {
-				this.setupPrettyPhoto();
-				this.setupAnnotorious();
-			} else {
-				this.removeLightboxLinks();
-			}
-		},
+      if ( self.options.add_embedly ) {
+        europeana.embedly.manageEmbedly( $('#inline-' + self.current ), $lightbox_description, self.current );
+      }
+    },
 
-		removeLightboxLinks : function() {
-			this.options.$elm.find('a').each(function() {
-				var $elm = jQuery(this),
-						contents = $elm.contents();
+    hideLightboxContent: function() {
+      var $pp_pic_holder = $('.pp_pic_holder');
+      $pp_pic_holder.find('#pp_full_res object,#pp_full_res embed').css('visibility','hidden');
+      $pp_pic_holder.find('.pp_fade').fadeOut('fast',function(){
+        $('.pp_loaderIcon').show();
+      });
+    },
 
-				if ( !$elm.hasClass('pdf') && !$elm.hasClass('original-context') ) {
-					$elm.replaceWith(contents);
-				}
-			});
+    /**
+     * @param {bool} options
+     */
+    init : function( options ) {
+      this.options = $.extend( true, {}, this.options, options );
+      alert('init lightbox');
 
-			this.options.$elm.find('.view-item').each(function() {
-				jQuery(this).remove();
-			});
-		},
+      if ( this.options.add_lightbox ) {
+        this.setupPrettyPhoto();
+        this.setupAnnotorious();
+      } else {
+        this.removeLightboxLinks();
+      }
+    },
 
-		removeMediaElementPlayers : function() {
-			var i;
+    removeLightboxLinks : function() {
+      this.options.$elm.find('a').each(function() {
+        var $elm = jQuery(this),
+            contents = $elm.contents();
 
-			if ( window.mejs === undefined ) {
-				return;
-			}
+        if ( !$elm.hasClass('pdf') && !$elm.hasClass('original-context') ) {
+          $elm.replaceWith(contents);
+        }
+      });
 
-			for ( i in mejs.players ) {
-				if ( mejs.players.hasOwnProperty(i) ) {
-					mejs.players[i].remove();
-				}
-			}
+      this.options.$elm.find('.view-item').each(function() {
+        jQuery(this).remove();
+      });
+    },
 
-			mejs.mepIndex = 0;
-		},
+    removeMediaElementPlayers : function() {
+      var i;
 
-		setupAnnotorious : function() {
-			if ( this.annotorious_setup ) {
-				return;
-			}
+      if ( window.mejs === undefined ) {
+        return;
+      }
 
-			if ( this.options.contribution_page ) {
-				anno.addPlugin( 'RunCoCo_Attachment', {} );
-			}
+      for ( i in mejs.players ) {
+        if ( mejs.players.hasOwnProperty(i) ) {
+          mejs.players[i].remove();
+        }
+      }
 
-			if ( this.options.edm_page ) {
-				anno.addPlugin( 'RunCoCo_EDM', {} );
-			}
+      mejs.mepIndex = 0;
+    },
 
-			anno.addPlugin(
-				'RunCoCo',
-				{
-					base_url :
-						window.location.protocol + "//" +
-						window.location.host + "/" +
-						RunCoCo.locale +
-						"/annotations"
-				}
-			);
+    setupAnnotorious : function() {
+      if ( this.annotorious_setup ) {
+        return;
+      }
+      alert('setupAnnotorious')
 
-			anno.addPlugin(
-				'Flag',
-				{
-					base_url :
-						window.location.protocol + "//" +
-						window.location.host + "/" +
-							RunCoCo.locale + "/annotations"
-				}
-			);
+      if ( this.options.contribution_page ) {
+        anno.addPlugin( 'RunCoCo_Attachment', {} );
+      }
 
-			this.annotorious_setup = true;
-		},
+      if ( this.options.edm_page ) {
+        anno.addPlugin( 'RunCoCo_EDM', {} );
+      }
 
-		setupPrettyPhoto : function() {
-			var self = this;
+      anno.addPlugin(
+        'RunCoCo',
+        {
+          base_url :
+            window.location.protocol + "//" +
+            window.location.host + "/" +
+            RunCoCo.locale +
+            "/annotations"
+        }
+      );
 
-			// called when the lightbox closes
-			self.ppOptions.callback = function() {
-				self.removeMediaElementPlayers();
+      anno.addPlugin(
+        'Flag',
+        {
+          base_url :
+            window.location.protocol + "//" +
+            window.location.host + "/" +
+              RunCoCo.locale + "/annotations"
+        }
+      );
 
-				// re-init of the lightbox insures that any additional content that
-				// was loaded while the lightbox was displayed is lightbox enabled
-				// when the lightbox is closed
-				self.init();
-			};
+      this.annotorious_setup = true;
+    },
 
-			self.ppOptions.changepagenext = self.handlePageChangeNext;
-			self.ppOptions.changepageprev = self.handlePageChangePrev;
-			self.ppOptions.changepicturecallback = self.handlePictureChange;
-			self.ppOptions.collection_total = self.options.carousel.pagination_total;
-			self.ppOptions.description_src = 'data-description';
-			self.ppOptions.image_markup = '<img id="fullResImage" src="{path}" class="annotatable">';
-			self.ppOptions.overlay_gallery = false;
-			self.ppOptions.show_title = false;
-			self.ppOptions.social_tools = false;
+    setupPrettyPhoto : function() {
+      var self = this;
 
-			self.options.$elm.find("a[rel^='prettyPhoto']").prettyPhoto( self.ppOptions );
-		}
+      // called when the lightbox closes
+      self.ppOptions.callback = function() {
+        self.removeMediaElementPlayers();
 
-	};
+        // re-init of the lightbox insures that any additional content that
+        // was loaded while the lightbox was displayed is lightbox enabled
+        // when the lightbox is closed
+        self.init();
+      };
+
+      self.ppOptions.changepagenext = self.handlePageChangeNext;
+      self.ppOptions.changepageprev = self.handlePageChangePrev;
+      self.ppOptions.changepicturecallback = self.handlePictureChange;
+      self.ppOptions.collection_total = self.options.carousel.pagination_total;
+      self.ppOptions.description_src = 'data-description';
+      self.ppOptions.image_markup = '<img id="fullResImage" src="{path}" class="annotatable">';
+      self.ppOptions.overlay_gallery = false;
+      self.ppOptions.show_title = false;
+      self.ppOptions.social_tools = false;
+
+      self.options.$elm.find("a[rel^='prettyPhoto']").prettyPhoto( self.ppOptions );
+    }
+
+  };
 
 }( jQuery ));
