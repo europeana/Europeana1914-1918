01045120dee48d8a4017af951ccb8c3803f41eff
andyjmaclean <andyjmaclean@gmail.com>
2016-10-14 16:04:07 +0200

    Re: #1393 test

--- app/assets/javascripts/europeana/lightbox.js
+++ app/assets/javascripts/europeana/lightbox.js
@@ -113,13 +113,21 @@
       var userLoggedIn = $('#navigation-user .username').length > 0;
       if(!userLoggedIn){
         $('.annotorious-annotationlayer').append(
-          '<span style="position: absolute; top:0.25em; border-radius:0.25em; left:0.25em; padding: 0.5em 0.75em 1em 0; background-color: rgba(0,0,0,0.45); color: white;">'
+          '<span id="login-to-annotate" style="position: absolute; top:0.25em; border-radius:0.25em; left:0.25em; padding: 0.25em 0.75em 1em 0; background-color: rgba(0,0,0,0.45); color: white;">'
           + '<span class="annotorious-hint-icon" style="width: auto; position: relative; padding-left: 1.25em; left: 3px;">'
           +   'Log in to annotate'
           + '</span>'
           + '</span>'
         );
       }
+
+      anno.addHandler('onMouseOverItem', function(annotation){
+        console.log('mouse over...');
+        if(userLoggedIn){
+          $('#login-to-annotate').remove();
+        }
+      });
+
     },
 
     hideLightboxContent: function() {
