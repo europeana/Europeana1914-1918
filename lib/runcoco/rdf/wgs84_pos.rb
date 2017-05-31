270d33a6b30cde88a4b6303350e18bf7df153c97
Richard Doe <richard.doe@rwdit.net>
2017-05-31 14:03:48 +0100

    Correct lat/lng/alt properties

--- lib/runcoco/rdf/wgs84_pos.rb
+++ lib/runcoco/rdf/wgs84_pos.rb
@@ -2,11 +2,11 @@ module RDF
   ##
   # WGS84 Geo Positioning vocabulary.
   class WGS84Pos < Vocabulary("http://www.w3.org/2003/01/geo/wgs84_pos#")
-    property :altitude
-    property :latitude
+    property :alt
+    property :lat
     property :lat_long
     property :location
-    property :longitude
+    property :long
     property :Point
     property :SpatialThing
   end
