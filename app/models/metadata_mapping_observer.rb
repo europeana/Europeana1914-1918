a4841eff5283216b932abe4d19e1e31e2f5ad135
Richard Doe <richard.doe@rwdit.net>
2017-06-06 14:45:06 +0200

    Observe metadata record model too

--- app/models/metadata_mapping_observer.rb
+++ app/models/metadata_mapping_observer.rb
@@ -2,22 +2,34 @@
 # Observer to (re)generate EDM mapping for contributions when they, or their
 # attachments or contributor are updated.
 class MetadataMappingObserver < ActiveRecord::Observer
-  observe :contribution, :attachment, :user
+  observe :contribution, :attachment, :metadata_record, :user
 
   def after_save(model)
-    case model
-    when Contribution
-      queue_job_for_contribution(model)
-    when Attachment
-      queue_job_for_contribution(model.contribution)
-    when User
-      model.contributions.find_each do |contribution|
+    subject = contribution_from_observed_model(model)
+
+    if subject.respond_to?(:find_each)
+      subject.find_each do |contribution|
         queue_job_for_contribution(contribution)
       end
+    else
+      queue_job_for_contribution(subject)
     end
   end
 
 protected
+
+  def contribution_from_observed_model(model)
+    case model
+    when Contribution
+      model
+    when Attachment
+      model.contribution
+    when MetadataRecord
+      model.contribution.present? ? model.contribution : model.attachment.contribution
+    when User
+      model.contributions
+    end
+  end
 
   def queue_job_for_contribution(contribution)
     return unless contribution.published?
