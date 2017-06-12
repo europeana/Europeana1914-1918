fc84f609048a3962e341fffc4f914ab63ad8293d
Richard Doe <richard.doe@rwdit.net>
2017-06-12 16:32:32 +0100

    Link to contributions if admin

--- app/helpers/contributions_helper.rb
+++ app/helpers/contributions_helper.rb
@@ -85,7 +85,8 @@ module ContributionsHelper
     elsif field_name == 'cataloguer'
       contribution.cataloguer ? contribution.cataloguer.contact.full_name : ''
     elsif field_name == 'title'
-      (contribution.title.present? ? truncate(contribution.title, :length => 50) : raw('<em>' + t('views.contributions.no_title') + '</em>'))
+      title_text = (contribution.title.present? ? truncate(contribution.title, :length => 50) : raw('<em>' + t('views.contributions.no_title') + '</em>'))
+      current_user.may_administer_contributions? ? link_to(title_text, contribution) : title_text
     elsif field_name == 'created_at'
       l contribution.created_at, :format => :timestamp
     elsif contribution.respond_to? field_name
