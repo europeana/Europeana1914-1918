55b311374feb34e3d406a65211a54c936c289a04
Richard Doe <richard.doe@rwdit.net>
2019-02-12 14:32:31 +0000

    Do not reinit dependencies

--- docker/init-db.sh
+++ docker/init-db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker-compose up -d db search
-docker-compose run web bundle exec rake db:seed
-docker-compose run web bundle exec rake sunspot:reindex
+docker-compose run --no-deps web bundle exec rake db:seed
+docker-compose run --no-deps web bundle exec rake sunspot:reindex
 docker-compose down
