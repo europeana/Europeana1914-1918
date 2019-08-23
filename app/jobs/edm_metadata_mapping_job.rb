d4d47e9cd0d8f705c6dbacf7f9d9827e1098cbe2
Richard Doe <richard.doe@rwdit.net>
2019-08-23 10:25:28 +0100

    Run attachment mappings as own jobs

--- app/jobs/edm_metadata_mapping_job.rb
+++ app/jobs/edm_metadata_mapping_job.rb
@@ -1,25 +1,26 @@
 ##
 # Job to generate and store EDM metadata mapping for a contribution
 class EDMMetadataMappingJob
-  def initialize(contribution_id)
-    @contribution_id = contribution_id
+  def initialize(resource_id, resource_class = 'Contribution')
+    @resource_id = resource_id
+    @resource_class = resource_class
   end
 
   def perform
-    mapping = MetadataMapping.where(:mappable_type => 'Contribution', :mappable_id => contribution.id, :format => 'edm_rdfxml').first_or_initialize
-    mapping.content = contribution.edm.to_rdfxml
+    mapping = MetadataMapping.where(:mappable_type => @resource_class, :mappable_id => resource.id, :format => 'edm_rdfxml').first_or_initialize
+    mapping.content = resource.edm.to_rdfxml
     mapping.save
 
-    if contribution.attachments_have_rich_metadata?
-      contribution.attachments.includes(:metadata => :taxonomy_terms).find_each do |attachment|
-        a_mapping = MetadataMapping.where(:mappable_type => 'Attachment', :mappable_id => attachment.id, :format => 'edm_rdfxml').first_or_initialize
-        a_mapping.content = attachment.edm.to_rdfxml
-        a_mapping.save
+    return unless @resource_class == 'Contribution'
+
+    if resource.attachments_have_rich_metadata?
+      resource.attachments.find_each do |attachment|
+        Delayed::Job.enqueue(self.class.new(attachment.id, 'Attachment'), :queue => 'mapping')
       end
     end
   end
 
-  def contribution
-    @contribution ||= Contribution.find(@contribution_id)
+  def resource
+    @resource ||= Object.const_get(@resource_class).find(@resource_id)
   end
 end
